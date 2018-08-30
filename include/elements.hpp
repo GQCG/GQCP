e234873dd649fb0b98b9348c4ff9c812c8f46e4f
Laurent Lemmens <laurent.lemmens@ugent.be>
2018-08-30 16:55:40 +0200

    Implement elementToAtomicNumber and atomicNumberToElement.

--- include/elements.hpp
+++ include/elements.hpp
@@ -12,7 +12,13 @@ namespace elements {
 /**
  *  Given a @param symbol for the name of an element, @return its atomic number
  */
-size_t element_to_atomic_number(const std::string& symbol);
+size_t elementToAtomicNumber(const std::string& symbol);
+
+
+/**
+ *  Given an @param atomic_number, @return the symbol of the corresponding element
+ */
+const std::string& atomicNumberToElement(size_t atomic_number);
 
 
 
