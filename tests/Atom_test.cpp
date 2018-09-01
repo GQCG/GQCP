97b20e4d5e379bec8d964c7d8012a349b5dd23a8
Laurent Lemmens <laurent.lemmens@ugent.be>
2018-09-01 10:34:23 +0200

    Implement operator< and operator== with a constexpr static tolerance for GQCG::Atom.

--- tests/Atom_test.cpp
+++ tests/Atom_test.cpp
@@ -14,7 +14,7 @@ BOOST_AUTO_TEST_CASE ( Atom_constructor ) {
 }
 
 
-BOOST_AUTO_TEST_CASE ( Atom_isEqualTo ) {
+BOOST_AUTO_TEST_CASE ( Atom_operator_equals ) {
 
     GQCG::Atom atom1 {1, 0.0, 0.1, 0.2};
     GQCG::Atom atom2 {1, 0.0, 0.1, 0.2};
@@ -22,16 +22,28 @@ BOOST_AUTO_TEST_CASE ( Atom_isEqualTo ) {
     GQCG::Atom atom4 {1, 0.1, 0.2, 0.3};
 
     // Check if they're equal
-    BOOST_CHECK(atom1.isEqualTo(atom2));
+    BOOST_CHECK(atom1 == atom2);
 
     // Check if different atomic numbers cause inequality
-    BOOST_CHECK(!atom1.isEqualTo(atom3));
+    BOOST_CHECK(!(atom1 == atom3));
 
     // Check if different coordinates cause inequality
-    BOOST_CHECK(!atom1.isEqualTo(atom4));
+    BOOST_CHECK(!(atom1 == atom4));
+}
 
-    // Check if the tolerance works as expected
-    BOOST_CHECK(atom1.isEqualTo(atom4, 0.5));
+
+BOOST_AUTO_TEST_CASE ( Atom_operator_smaller_than ) {
+
+    GQCG::Atom atom1 {1, 0.0, 0.1, 0.2};
+    GQCG::Atom atom2 {2, 0.0, 0.1, 0.2};
+    GQCG::Atom atom3 {2, 0.1, 0.2, 0.2};
+    GQCG::Atom atom4 {2, 0.1, 0.2, 0.3};
+
+
+    // Check if operator< does what is expected
+    BOOST_CHECK(atom1 < atom2);
+    BOOST_CHECK(atom2 < atom3);
+    BOOST_CHECK(atom3 < atom4);
 }
 
 
