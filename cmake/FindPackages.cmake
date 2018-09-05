e320de8aa349d6483687108fd9efa6a1e47a910d
Laurent Lemmens <laurent.lemmens@ugent.be>
2018-09-05 10:52:10 +0200

    Remove unnecessary boost components.

--- cmake/FindPackages.cmake
+++ cmake/FindPackages.cmake
@@ -1,8 +1,8 @@
 # In this CMake file, we will find all required packages
 
 
-# Find the Boost package and some required components
-find_package(Boost REQUIRED COMPONENTS system thread program_options)
+# Find the Boost package
+find_package(Boost REQUIRED)
 
 # Find Eigen3
 find_package(Eigen3 3.3.4 REQUIRED)
