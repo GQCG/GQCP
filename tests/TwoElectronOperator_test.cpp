acf0b389d18c7ad209b3ddcc4caf840f74bd2b09
Laurent Lemmens <laurent_lemmens@hotmail.com>
2018-09-10 18:42:45 +0200

    Implement two-electron rotation reference test.

--- tests/TwoElectronOperator_test.cpp
+++ tests/TwoElectronOperator_test.cpp
@@ -18,7 +18,8 @@ BOOST_AUTO_TEST_CASE ( TwoElectronOperator_constructor ) {
 
     // Check a faulty constructor
     Eigen::Tensor<double, 4> tensor2 (3, 3, 3, 2);
-    BOOST_CHECK_THROW(GQCG::TwoElectronOperator O2 (tensor2), std::invalid_argument); }
+    BOOST_CHECK_THROW(GQCG::TwoElectronOperator O2 (tensor2), std::invalid_argument);
+}
 
 
 BOOST_AUTO_TEST_CASE ( TwoElectronOperator_getters ) {
@@ -41,4 +42,31 @@ BOOST_AUTO_TEST_CASE ( TwoElectronOperator_transform_trivial ) {
     G.transform(T);
 
     BOOST_CHECK(cpputil::linalg::areEqual(g, G.get_matrix_representation(), 1.0e-12));
+}
+
+
+BOOST_AUTO_TEST_CASE ( TwoElectronOperator_transform_olsens ) {
+
+    // We can find a reference algorithm in the olsens module from Ayer's lab
+    Eigen::Tensor<double, 4> g_transformed_ref (2, 2, 2, 2);
+    cpputil::io::readArrayFromFile("../tests/data/rotated_two_electron_integrals_olsens.data", g_transformed_ref);
+
+    // Set an example transformation matrix and two-electron integrals tensor
+    Eigen::MatrixXd T (2, 2);
+    T << 1, 2, 3, 4;
+
+    Eigen::Tensor<double, 4> g (2, 2, 2, 2);
+    for (size_t i = 0; i < 2; i++) {
+        for (size_t j = 0; j < 2; j++) {
+            for (size_t k = 0; k < 2; k++) {
+                for (size_t l = 0; l < 2; l++) {
+                    g(i, j, k, l) = l + 2*k + 4*j + 8*i;
+                }
+            }
+        }
+    }
+    GQCG::TwoElectronOperator G (g);
+    G.transform(T);
+
+//    BOOST_CHECK(cpputil::linalg::areEqual(G.get_matrix_representation(), g_transformed_ref, 1.0e-12));
 }
