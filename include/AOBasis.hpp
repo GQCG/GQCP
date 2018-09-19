576159b658fbc3d67705dd27e881da3813ff630d
Laurent Lemmens <laurent_lemmens@hotmail.com>
2018-09-19 10:47:22 +0200

    Implement AOBasis constructor and AOBasis.number_of_basis_functions.

--- include/AOBasis.hpp
+++ include/AOBasis.hpp
@@ -15,18 +15,21 @@ namespace GQCG {
 
 
 /**
- *  A class that represents an atomic orbital basis, with the members
- *      - @member basis_functions
- *      - @member S, which is the overlap matrix of the @member basis_functions
+ *  A class that represents an atomic orbital basis
  */
 class AOBasis {
 private:
     const std::vector<GQCG::Atom> atoms;
     const libint2::BasisSet basis_functions;
+    const size_t number_of_basis_functions;
 
 public:
     // CONSTRUCTOR
     AOBasis(const GQCG::Molecule& molecule, std::string basis_set);
+
+
+    // GETTERS
+    size_t get_number_of_basis_functions() const { return this->number_of_basis_functions; }
 
 
     // FRIEND CLASSES
--- src/AOBasis.cpp
+++ src/AOBasis.cpp
@@ -6,10 +6,9 @@ namespace GQCG {
 
 AOBasis::AOBasis(const GQCG::Molecule& molecule, std::string basis_set) :
     atoms (molecule.atoms),
-    basis_functions (libint2::BasisSet(std::move(basis_set), GQCG::LibintCommunicator::get().interface(this->atoms)))
-{
-    //
-}
+    basis_functions (libint2::BasisSet(std::move(basis_set), GQCG::LibintCommunicator::get().interface(this->atoms))),  // construct a libint2::BasisSet
+    number_of_basis_functions (this->basis_functions.nbf())
+{}
 
 
 }  // namespace GQCG
