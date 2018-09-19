d3a6ba26e0f90d5a7232da88912e6afa8a9cd2b8
Laurent Lemmens <laurent_lemmens@hotmail.com>
2018-09-19 10:48:05 +0200

    Add static_cast to size_t.

--- src/AOBasis.cpp
+++ src/AOBasis.cpp
@@ -7,7 +7,7 @@ namespace GQCG {
 AOBasis::AOBasis(const GQCG::Molecule& molecule, std::string basis_set) :
     atoms (molecule.atoms),
     basis_functions (libint2::BasisSet(std::move(basis_set), GQCG::LibintCommunicator::get().interface(this->atoms))),  // construct a libint2::BasisSet
-    number_of_basis_functions (this->basis_functions.nbf())
+    number_of_basis_functions (static_cast<size_t>(this->basis_functions.nbf()))
 {}
 
 
