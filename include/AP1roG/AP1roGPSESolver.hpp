27cf227074180ef8a48e23892e11b2885162b6cc
Laurent Lemmens <laurent_lemmens@hotmail.com>
2018-10-08 13:31:30 +0200

    Add constructor to AP1roGPSESolver.

--- include/AP1roG/AP1roGPSESolver.hpp
+++ include/AP1roG/AP1roGPSESolver.hpp
@@ -1,14 +1,41 @@
-//
-//  AP1roGPSESolver.hpp
-//  gqcg
-//
-//  Created by Laurent Lemmens on 08/10/2018.
-//  Copyright © 2018 Ghent Quantum Chemistry Group. All rights reserved.
-//
-
 #ifndef AP1roGPSESolver_hpp
 #define AP1roGPSESolver_hpp
 
-#include <stdio.h>
+
+#include "HamiltonianParameters/HamiltonianParameters.hpp"
+#include "Molecule.hpp"
+#include "AP1roG/AP1roGGeminalCoefficients.hpp"
+#include "AP1roG/AP1roG.hpp"
+
+namespace GQCG {
+
+/**
+ *
+ */
+class AP1roGPSESolver {
+private:
+    const size_t K;  // the number of special orbitals
+    const size_t N_P;  // the number of electron pairs
+    const GQCG::AP1roGGeminalCoefficients initial_geminal_coefficients;
+    
+    GQCG::HamiltonianParameters ham_par;
+
+    GQCG::AP1roG solution;
+
+
+public:
+    // CONSTRUCTORS
+    /**
+     *  Constructor based on a given @param molecule and Hamiltonian parameters @param ham_par
+     *
+     *  The initial guess for the geminal coefficients is zero
+     */
+    AP1roGPSESolver(const GQCG::Molecule& molecule, const GQCG::HamiltonianParameters& ham_par);
+};
+
+
+}  // namespace GQCG
+
+
 
 #endif /* AP1roGPSESolver_hpp */
--- src/AP1roG/AP1roGPSESolver.cpp
+++ src/AP1roG/AP1roGPSESolver.cpp
@@ -1,9 +1,20 @@
-//
-//  AP1roGPSESolver.cpp
-//  gqcg
-//
-//  Created by Laurent Lemmens on 08/10/2018.
-//  Copyright © 2018 Ghent Quantum Chemistry Group. All rights reserved.
-//
-
 #include "AP1roGPSESolver.hpp"
+
+
+namespace GQCG {
+
+
+AP1roGPSESolver::AP1roGPSESolver(const GQCG::Molecule& molecule, const GQCG::HamiltonianParameters& ham_par) :
+    K (ham_par.K),
+    ham_par (ham_par),
+    N_P (molecule.N / 2),
+    initial_geminal_coefficients (GQCG::AP1roGGeminalCoefficients(this->N_P, this->K))
+{
+    // Check if we have an even number of electrons
+    if ((molecule.N % 2) != 0) {
+        throw std::invalid_argument("The given molecule has an odd number of electrons.");
+    }
+}
+
+
+}  // namespace GQCG
