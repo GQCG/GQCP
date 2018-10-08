b71453dd606ef97e1e3c25a536d7705cd4181f7a
Laurent Lemmens <laurent_lemmens@hotmail.com>
2018-10-08 10:18:30 +0200

    Add folder structure.

---
+++ include/AP1roG/AP1roGPSESolver.hpp
@@ -0,0 +1,14 @@
+//
+//  AP1roGPSESolver.hpp
+//  gqcg
+//
+//  Created by Laurent Lemmens on 08/10/2018.
+//  Copyright © 2018 Ghent Quantum Chemistry Group. All rights reserved.
+//
+
+#ifndef AP1roGPSESolver_hpp
+#define AP1roGPSESolver_hpp
+
+#include <stdio.h>
+
+#endif /* AP1roGPSESolver_hpp */
---
+++ src/AP1roG/AP1roGPSESolver.cpp
@@ -0,0 +1,9 @@
+//
+//  AP1roGPSESolver.cpp
+//  gqcg
+//
+//  Created by Laurent Lemmens on 08/10/2018.
+//  Copyright © 2018 Ghent Quantum Chemistry Group. All rights reserved.
+//
+
+#include "AP1roGPSESolver.hpp"
