27ab3e2ebef6b0e22f8405a5c786ddd550d45417
Laurent Lemmens <laurent_lemmens@hotmail.com>
2018-10-19 14:45:13 +0200

    Perform the requested changes. Change namespace to GQCP.

--- include/OrbitalOptimizationOptions.hpp
+++ include/OrbitalOptimizationOptions.hpp
@@ -8,7 +8,7 @@ namespace GQCP {
     struct OrbitalOptimizationOptions {
         double convergence_threshold = 1.0e-08;
         size_t maximum_number_of_iterations = 128;
-    }
+    };
 
 
 }  // namespace GQCP
