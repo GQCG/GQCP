5a5c55361523ff943ca110de433e608b2fb8d7bc
Laurent Lemmens <laurent_lemmens@hotmail.com>
2018-10-19 11:18:53 +0200

    Add copyright headers.

--- src/RHF/DIISRHFSCFSolver.cpp
+++ src/RHF/DIISRHFSCFSolver.cpp
@@ -1,11 +1,20 @@
-//
-//  DIISRHFSCFSolver.cpp
-//  GQCP
-//
-//  Created by Laurent Lemmens on 04/10/2018.
-//  Copyright © 2018 Ghent Quantum Chemistry Group. All rights reserved.
-//
-
+// This file is part of GQCG-gqcp.
+// 
+// Copyright (C) 2017-2018  the GQCG developers
+// 
+// GQCG-gqcp is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// GQCG-gqcp is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with GQCG-gqcp.  If not, see <http://www.gnu.org/licenses/>.
+// 
 #include "RHF/DIISRHFSCFSolver.hpp"
 
 
