11a4d4276843f881b7e4e7c90541e15e65467354
Laurent Lemmens <laurent_lemmens@hotmail.com>
2018-10-24 12:13:08 +0200

    Add copyright headers. Bring up to v0.1.0.

--- src/RDM/RDMCalculator.cpp
+++ src/RDM/RDMCalculator.cpp
@@ -14,7 +14,7 @@
 // 
 // You should have received a copy of the GNU Lesser General Public License
 // along with GQCG-gqcp.  If not, see <http://www.gnu.org/licenses/>.
-//
+// 
 #include "RDM/RDMCalculator.hpp"
 
 #include "RDM/DOCIRDMBuilder.hpp"
--- src/WaveFunction/WaveFunctionReader.cpp
+++ src/WaveFunction/WaveFunctionReader.cpp
@@ -14,8 +14,7 @@
 // 
 // You should have received a copy of the GNU Lesser General Public License
 // along with GQCG-gqcp.  If not, see <http://www.gnu.org/licenses/>.
-//
-
+// 
 #include <fstream>
 
 #include <boost/algorithm/string.hpp>
