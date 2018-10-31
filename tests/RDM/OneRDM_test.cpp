7e1a88c61affe3e4635b8b98f0cfff5883a55c9e
Laurent Lemmens <laurent_lemmens@hotmail.com>
2018-10-31 11:33:41 +0100

    Implement 1- and 2-RDM constructor tests.

--- tests/RDM/OneRDM_test.cpp
+++ tests/RDM/OneRDM_test.cpp
@@ -1,9 +1,37 @@
-//
-//  OneRDM_test.cpp
-//  gqcp
-//
-//  Created by Laurent Lemmens on 31/10/2018.
-//  Copyright © 2018 Ghent Quantum Chemistry Group. All rights reserved.
-//
+// This file is part of GQCG-gqcp.
+// 
+// Copyright (C) 2017-2018  the GQCG developers
+// 
+// GQCG-gqcp is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// GQCG-gqcp is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with GQCG-gqcp.  If not, see <http://www.gnu.org/licenses/>.
+// 
+#define BOOST_TEST_MODULE "OneRDM"
 
-#include <stdio.h>
+
+#include "RDM/OneRDM.hpp"
+
+#include <boost/test/unit_test.hpp>
+#include <boost/test/included/unit_test.hpp>  // include this to get main(), otherwise the compiler will complain
+
+
+BOOST_AUTO_TEST_CASE ( OneRDM_constructor ) {
+
+    // Check a correct constructor
+    Eigen::MatrixXd matrix = Eigen::MatrixXd::Zero(4, 4);
+    GQCP::OneRDM D (matrix);
+
+
+    // Check a faulty constructor
+    Eigen::MatrixXd matrix2 = Eigen::MatrixXd::Zero(3, 4);
+    BOOST_CHECK_THROW(GQCP::OneRDM D2 (matrix2), std::invalid_argument);
+}
--- tests/RDM/TwoRDM_test.cpp
+++ tests/RDM/TwoRDM_test.cpp
@@ -1,9 +1,37 @@
-//
-//  TwoRDM_test.cpp
-//  gqcp
-//
-//  Created by Laurent Lemmens on 31/10/2018.
-//  Copyright © 2018 Ghent Quantum Chemistry Group. All rights reserved.
-//
+// This file is part of GQCG-gqcp.
+// 
+// Copyright (C) 2017-2018  the GQCG developers
+// 
+// GQCG-gqcp is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// GQCG-gqcp is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with GQCG-gqcp.  If not, see <http://www.gnu.org/licenses/>.
+// 
+#define BOOST_TEST_MODULE "TwoRDM"
 
-#include <stdio.h>
+
+#include "RDM/TwoRDM.hpp"
+
+#include <boost/test/unit_test.hpp>
+#include <boost/test/included/unit_test.hpp>  // include this to get main(), otherwise the compiler will complain
+
+
+BOOST_AUTO_TEST_CASE ( TwoRDM_constructor ) {
+
+    // Check a correct constructor
+    Eigen::Tensor<double, 4> tensor (3, 3, 3, 3);
+    GQCP::TwoRDM d (tensor);
+
+
+    // Check a faulty constructor
+    Eigen::Tensor<double, 4> tensor2 (3, 3, 3, 2);
+    BOOST_CHECK_THROW(GQCP::TwoRDM d2 (tensor2), std::invalid_argument);
+}
