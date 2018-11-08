3886045738bd89d480237e861f6cd811bf23de3b
Laurent Lemmens <laurent_lemmens@hotmail.com>
2018-11-08 11:11:04 +0100

    Implement the localization algorithm.

--- tests/Localization/ERJacobiLocalizer_test.cpp
+++ tests/Localization/ERJacobiLocalizer_test.cpp
@@ -15,10 +15,13 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with GQCG-gqcp.  If not, see <http://www.gnu.org/licenses/>.
 // 
-#define BOOST_TEST_MODULE "ERLocalizer"
+#define BOOST_TEST_MODULE "ERJacobiLocalizer"
 
 
-#include "Localization/ERLocalizer.hpp"
+#include "Localization/ERJacobiLocalizer.hpp"
+
+#include "HamiltonianParameters/HamiltonianParameters_constructors.hpp"
+
 
 #include <boost/test/unit_test.hpp>
 #include <boost/test/included/unit_test.hpp>  // include this to get main(), otherwise the compiler will complain
@@ -26,5 +29,17 @@
 
 BOOST_AUTO_TEST_CASE ( sandbox ) {
 
+    GQCP::Molecule h2o ("../tests/data/h2o.xyz");
+    auto ao_basis = std::make_shared<GQCP::AOBasis>(h2o, "STO-3G");
+    auto mol_ham_par = GQCP::constructMolecularHamiltonianParameters(ao_basis);  // in AOBasis
+    mol_ham_par.LowdinOrthonormalize();  // in the Löwdin basis
 
+
+    GQCP::ERJacobiLocalizer localizer (h2o.get_N()/2);
+    double index = localizer.calculateLocalizationIndex(mol_ham_par);
+    std::cout << index << std::endl;
+
+    localizer.localize(mol_ham_par);  // now the Hamiltonian parameters are in the localized basis
+    index = localizer.calculateLocalizationIndex(mol_ham_par);
+    std::cout << index << std::endl;
 }
