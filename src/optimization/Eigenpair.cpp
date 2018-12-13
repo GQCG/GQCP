7607959db1028a701ed2e7b5d4391873dbe402f6
Laurent Lemmens <laurent_lemmens@hotmail.com>
2018-12-13 16:43:33 +0100

    Include all files into the main library.

--- src/optimization/Eigenpair.cpp
+++ src/optimization/Eigenpair.cpp
@@ -1,27 +1,27 @@
-// This file is part of GQCG-numopt.
+// This file is part of GQCG-gqcp.
 // 
 // Copyright (C) 2017-2018  the GQCG developers
 // 
-// GQCG-numopt is free software: you can redistribute it and/or modify
+// GQCG-gqcp is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 // 
-// GQCG-numopt is distributed in the hope that it will be useful,
+// GQCG-gqcp is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU Lesser General Public License for more details.
 // 
 // You should have received a copy of the GNU Lesser General Public License
-// along with GQCG-numopt.  If not, see <http://www.gnu.org/licenses/>.
+// along with GQCG-gqcp.  If not, see <http://www.gnu.org/licenses/>.
+// 
 #include "Eigenpair.hpp"
 
-#include <cpputil.hpp>
+#include "utilities/linalg.hpp"
 
 
 
-namespace numopt {
-namespace eigenproblem {
+namespace GQCP {
 
 
 /*
@@ -68,7 +68,7 @@ bool Eigenpair::isEqual(const numopt::eigenproblem::Eigenpair& other, double tol
     }
 
     if (std::abs(this->eigenvalue - other.eigenvalue) < tolerance) {
-        if (cpputil::linalg::areEqualEigenvectors(this->eigenvector, other.get_eigenvector(), tolerance)) {
+        if (GQCP::areEqualEigenvectors(this->eigenvector, other.get_eigenvector(), tolerance)) {
             return true;
         }
     }
@@ -77,5 +77,4 @@ bool Eigenpair::isEqual(const numopt::eigenproblem::Eigenpair& other, double tol
 }
 
 
-}  // namespace eigenproblem
-}  // namespace numopt
+}  // namespace GQCP
--- src/optimization/SparseSolver.cpp
+++ src/optimization/SparseSolver.cpp
@@ -1,19 +1,20 @@
-// This file is part of GQCG-numopt.
+// This file is part of GQCG-gqcp.
 // 
 // Copyright (C) 2017-2018  the GQCG developers
 // 
-// GQCG-numopt is free software: you can redistribute it and/or modify
+// GQCG-gqcp is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 // 
-// GQCG-numopt is distributed in the hope that it will be useful,
+// GQCG-gqcp is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU Lesser General Public License for more details.
 // 
 // You should have received a copy of the GNU Lesser General Public License
-// along with GQCG-numopt.  If not, see <http://www.gnu.org/licenses/>.
+// along with GQCG-gqcp.  If not, see <http://www.gnu.org/licenses/>.
+// 
 #include "SparseSolver.hpp"
 
 #include <Spectra/SymEigsSolver.h>
