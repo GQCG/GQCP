3e0a5f3ff83b5f3c740ed71fc4e8ba818559ed09
tmhuysen <tobias.huysentruyt@minimoa.net>
2018-12-17 19:38:27 +0100

    fixed header

--- src/FockSpace/BaseFockSpace.cpp
+++ src/FockSpace/BaseFockSpace.cpp
@@ -14,13 +14,12 @@
 // 
 // You should have received a copy of the GNU Lesser General Public License
 // along with GQCG-gqcp.  If not, see <http://www.gnu.org/licenses/>.
-// 
-#include <FockSpace/BaseFockSpace.hpp>
-
+//
 #include "FockSpace/BaseFockSpace.hpp"
 #include "FockSpace/FockSpace.hpp"
 #include "FockSpace/ProductFockSpace.hpp"
 #include "FockSpace/SelectedFockSpace.hpp"
+
 
 namespace GQCP {
 
