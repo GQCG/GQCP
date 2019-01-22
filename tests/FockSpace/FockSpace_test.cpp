c94a5c54602da3f5dab2036778ec4a131b53a177
tmhuysen <tobias.huysentruyt@minimoa.net>
2019-01-22 17:03:49 +0100

    updated tests

--- tests/FockSpace/FockSpace_test.cpp
+++ tests/FockSpace/FockSpace_test.cpp
@@ -146,3 +146,83 @@ BOOST_AUTO_TEST_CASE ( iterateToNextUnoccupiedOrbital_signed ) {
     BOOST_CHECK(q == 4);
     BOOST_CHECK(sign == -1);
 }
+
+
+BOOST_AUTO_TEST_CASE ( shiftToPreviousOrbital_signed ) {
+
+    GQCP::FockSpace fock_space (5, 3);
+    GQCP::ONV onv = fock_space.makeONV(6);  // 10110
+
+    size_t address_shift = 0;
+    int sign = 1;
+
+    // test shift if we plan on creating one electron and start from orbital index 2
+    size_t e = 0;  // count starts at 0 (translates to orbital index 1)
+    size_t q = 1;  // index starts at orbital index 1
+
+    //  Index 1 is occupied an thus its weight shall shift if we create before an electron on a smaller index.
+    //  In this instance the electron weight at index 1 should be shifted.
+    //  Initial weight contribution is 1,
+    //  This should be shifted to 0, the difference is 1.
+    //  The sign changes (flips once)
+    fock_space.shiftUntilPreviousUnoccupiedOrbital<1>(onv, address_shift, q, e, sign);
+
+    BOOST_CHECK(address_shift == -1);
+    BOOST_CHECK(e == -1);
+    BOOST_CHECK(q == 0);
+    BOOST_CHECK(sign == -1);
+
+    sign = 1;
+    address_shift = 0;
+    onv = fock_space.makeONV(9);  // 11100
+    // test shift if we plan on creating two electrons and start from orbital index 2
+    e = 0;  // count starts at 1 (translates to orbital index 2)
+    q = 2;  // index starts at orbital index 2
+
+    //  Index 2 is occupied an thus its weight shall shift if we create before an electron on a smaller index.
+    //  In this instance the electron weight at index 2 should be shifted.
+    //  Initial weight contribution is 2,
+    //  This should be shifted to 0, the difference is 2.
+    //  The sign changes (flips once)
+    fock_space.shiftUntilPreviousUnoccupiedOrbital<2>(onv, address_shift, q, e, sign);
+
+    BOOST_CHECK(address_shift == -2);
+    BOOST_CHECK(e == -1);
+    BOOST_CHECK(q == 1);
+    BOOST_CHECK(sign == -1);
+}
+
+
+BOOST_AUTO_TEST_CASE ( coupling_count ) {
+
+    GQCP::FockSpace fock_space (5, 3);
+    GQCP::ONV onv = fock_space.makeONV(3);  // 01110
+
+    // We only count couplings with larger addresses
+
+    BOOST_CHECK(fock_space.countOneElectronCouplings(onv) == 3); // 11100, 11010, 10110
+    BOOST_CHECK(fock_space.countTwoElectronCouplings(onv) == 3+3);  // 11100, 11010, 10110, 11001, 10101, 10011
+
+    onv = fock_space.makeONV(0);  // 00111
+
+    BOOST_CHECK(fock_space.countOneElectronCouplings(onv) == 6);
+    BOOST_CHECK(fock_space.countTwoElectronCouplings(onv) == 6+3); // all of them
+
+
+    // test whether the total count matches that of individual counts of all ONVs in the Fock space.
+    GQCP::FockSpace fock_space2 (16,8);
+
+    size_t coupling_count1 = 0;
+    size_t coupling_count2 = 0;
+    onv = fock_space2.makeONV(0);  // spin string with address 0
+    for (size_t I = 0; I < fock_space2.get_dimension(); I++) {  // I_alpha loops over all addresses of alpha spin strings
+        if (I > 0) {
+            fock_space2.setNextONV(onv);
+        }
+        coupling_count1 += fock_space2.countOneElectronCouplings(onv);
+        coupling_count2 += fock_space2.countTwoElectronCouplings(onv);
+    }
+
+    BOOST_CHECK(2*coupling_count1 == fock_space2.countTotalOneElectronCouplings());
+    BOOST_CHECK(2*coupling_count2 == fock_space2.countTotalTwoElectronCouplings());
+}
