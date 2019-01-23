91002285f9d9964b93fd45cd2ebcfa51dc87f146
Laurent Lemmens <laurent_lemmens@hotmail.com>
2019-01-23 11:45:43 +0100

    Update cmake/Configure/ConfigureDriver.cmake
    Co-Authored-By: guacke <guacke@users.noreply.github.com>

--- cmake/Configure/ConfigureDriver.cmake
+++ cmake/Configure/ConfigureDriver.cmake
@@ -5,7 +5,6 @@ function(configure_driver DRIVER_NAME)
 
     configure_executable(${DRIVER_NAME})
 
-    #target_link_libraries(${DRIVER_NAME} PUBLIC "-lboost_program_options")
 
     # Install the driver in a separate location
     install(TARGETS ${DRIVER_NAME} RUNTIME DESTINATION ${BIN_INSTALL_DIR})
