92c915a96e9a2b3834be5805135289e1db598c5c
Laurent Lemmens <laurent_lemmens@hotmail.com>
2019-01-28 12:41:45 +0100

    Update copyright headers.

--- tests/RDM/SpinUnresolvedFCIRDMBuilder_test.cpp
+++ tests/RDM/SpinUnresolvedFCIRDMBuilder_test.cpp
@@ -14,7 +14,7 @@
 // 
 // You should have received a copy of the GNU Lesser General Public License
 // along with GQCG-gqcp.  If not, see <http://www.gnu.org/licenses/>.
-//
+// 
 #define BOOST_TEST_MODULE "UnresolvedCIRDMBuilder_test"
 
 
