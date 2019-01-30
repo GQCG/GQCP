41e28adb41a3b74fdfc506b4ec19909a5bd8506c
Laurent Lemmens <laurent_lemmens@hotmail.com>
2019-01-30 15:31:49 +0100

    Fix compiler options.

--- cmake/Definitions/CMakeDefinitions.cmake
+++ cmake/Definitions/CMakeDefinitions.cmake
@@ -14,6 +14,7 @@ if (NOT CMAKE_BUILD_TYPE)
 endif()
 message(STATUS "Building ${LIBRARY_NAME} in ${CMAKE_BUILD_TYPE} mode")
 
+include(${CMAKE_SOURCE_DIR}/cmake/Definitions/Compiler.cmake)
 
 include(${CMAKE_SOURCE_DIR}/cmake/Definitions/Options.cmake)
 
