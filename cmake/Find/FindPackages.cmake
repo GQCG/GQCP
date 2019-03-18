cb807a7cf4863879f9743963de89337f7d569f97
Guillaume Acke <Guillaume.Acke@UGent.be>
2019-03-18 14:52:22 +0100

    Eigen fix: no_module.

--- cmake/Find/FindPackages.cmake
+++ cmake/Find/FindPackages.cmake
@@ -3,7 +3,7 @@
 
 find_package(Git REQUIRED)
 find_package(Boost REQUIRED COMPONENTS program_options)
-find_package(Eigen3 3.3.4 REQUIRED)
+find_package(Eigen3 3.3.4 REQUIRED NO_MODULE)
 find_package(Libint2 REQUIRED)
 find_package(Spectra REQUIRED)
 
