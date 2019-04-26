380f36fdeeb73703188ad6c9b0336090fc59cea5
tmhuysen <tobias.huysentruyt@minimoa.net>
2019-04-26 17:30:04 +0200

    incorrect alt

--- include/HamiltonianParameters/AtomicDecompositionParameters.hpp
+++ include/HamiltonianParameters/AtomicDecompositionParameters.hpp
@@ -99,14 +99,14 @@ struct AtomicDecompositionParameters {
     };
 
     static AtomicDecompositionParameters ALT(const HamiltonianParameters<double>& mol_ham_par) {
-            if (!this->get_ao_basis()) {
+            if (!mol_ham_par.get_ao_basis()) {
                     throw std::invalid_argument("HamiltonianParameters::atomicDecomposition(): The Hamiltonian parameters have no underlying AO basis, atomic decomposition is not possible.");
             }
 
-            auto ham_par = *this;
-            ham_par.transform(this->T_total.inverse());
-
-            auto atoms = this->get_ao_basis().get_shell_set().atoms();
+            auto ham_par = mol_ham_par;
+            ham_par.transform<double>(mol_ham_par.get_T_total().inverse());
+            auto K = mol_ham_par.get_K();
+            auto atoms = mol_ham_par.get_ao_basis().get_shell_set().atoms();
 
             if (atoms.size() > 2) {
                     throw std::invalid_argument("HamiltonianParameters::atomicDecomposition(): The Hamiltonian parameters are set up for more than 2 atoms, currently only available for diatomic molecules");
@@ -115,15 +115,15 @@ struct AtomicDecompositionParameters {
             Molecule atom_a ({atoms[0]});
             Molecule atom_b ({atoms[1]});
 
-            auto basisset_name = this->get_ao_basis().get_shell_set().get_basisset_name();
-            AOBasis aobasis_a (atom_a, this->get_ao_basis().get_shell_set().get_basisset_name());
-            AOBasis aobasis_b (atom_b, this->get_ao_basis().get_shell_set().get_basisset_name());
+            auto basisset_name = mol_ham_par.get_ao_basis()->get_shell_set().get_basisset_name();
+            AOBasis aobasis_a (atom_a, mol_ham_par.get_ao_basis()->get_shell_set().get_basisset_name());
+            AOBasis aobasis_b (atom_b, mol_ham_par.get_ao_basis()->get_shell_set().get_basisset_name());
 
             auto Ka = aobasis_a.numberOfBasisFunctions();
             auto Kb = aobasis_b.numberOfBasisFunctions();
 
-            auto p_a = SquareMatrix::PartitionMatrix(0, Ka, this->K);
-            auto p_b = SquareMatrix::PartitionMatrix(Ka, Kb, this->K);
+            auto p_a = SquareMatrix<double>::PartitionMatrix(0, Ka, K);
+            auto p_b = SquareMatrix<double>::PartitionMatrix(Ka, Kb, K);
 
             auto v_a = aobasis_a.calculateNuclearIntegrals();
             auto v_b = aobasis_b.calculateNuclearIntegrals();
@@ -131,11 +131,11 @@ struct AtomicDecompositionParameters {
             auto t_a = aobasis_a.calculateKineticIntegrals();
             auto t_b = aobasis_b.calculateKineticIntegrals();
 
-            auto t_ab = this->ao_basis.calculateKineticIntegrals() - t_a - t_b;
-            auto v_ab = this->ao_basis.calculateNuclearIntegrals() - v_a - v_b;
+            auto t_ab = mol_ham_par.get_ao_basis()->calculateKineticIntegrals() - t_a - t_b;
+            auto v_ab = mol_ham_par.get_ao_basis()->calculateNuclearIntegrals() - v_a - v_b;
 
-            OneElectronOperator<double> h_a = OneElectronOperator<double>::Zero(this->K, this->K);
-            OneElectronOperator<double> h_b = OneElectronOperator<double>::Zero(this->K, this->K);
+            OneElectronOperator<double> h_a = OneElectronOperator<double>::Zero(K, K);
+            OneElectronOperator<double> h_b = OneElectronOperator<double>::Zero(K, K);
 
             h_a.block(0,0,Ka,Ka) = t_a + v_a;
             h_b.block(Ka,Ka,Kb,Kb) = t_b + v_b;
@@ -144,14 +144,14 @@ struct AtomicDecompositionParameters {
             auto g_b = ham_par.get_g();
             auto g_ab = ham_par.get_g();
 
-            g_a.fourModeMultiplication(p_a, SquareMatrix::Identiy(this->K, this->K), p_a, SquareMatrix::Identiy(this->K, this->K));
-            g_b.fourModeMultiplication(p_b, SquareMatrix::Identiy(this->K, this->K), p_b, SquareMatrix::Identiy(this->K, this->K));
-            g_ab.fourModeMultiplication(p_a, SquareMatrix::Identiy(this->K, this->K), p_b, SquareMatrix::Identiy(this->K, this->K));
+            g_a.fourModeMultiplication<double>(p_a, SquareMatrix<double>::Identity(K, K), p_a, SquareMatrix<double>::Identity(K, K));
+            g_b.fourModeMultiplication<double>(p_b, SquareMatrix<double>::Identity(K, K), p_b, SquareMatrix<double>::Identity(K, K));
+            g_ab.fourModeMultiplication<double>(p_a, SquareMatrix<double>::Identity(K, K), p_b, SquareMatrix<double>::Identity(K, K));
 
-            HamiltonianParameters<double> HAA(this->ao_basis, ham_par.get_S(), h_a, g_a, SquareMatrix::Identiy(this->K, this->K));
-            HamiltonianParameters<double> HBB(this->ao_basis, ham_par.get_S(), h_b, g_b, SquareMatrix::Identiy(this->K, this->K));
-            HamiltonianParameters<double> HAB(this->ao_basis, ham_par.get_S(), ham_par.get_h()-h_a-h_b, 2*g_ab, SquareMatrix::Identiy(this->K, this->K));
-            HamiltonianParameters<double> HA(this->ao_basis, ham_par.get_S(), (ham_par.get_h()-h_a-h_b)/2, g_ab, SquareMatrix::Identiy(this->K, this->K));
+            HamiltonianParameters<double> HAA(mol_ham_par.get_ao_basis(), ham_par.get_S(), h_a, g_a, SquareMatrix<double>::Identity(K, K));
+            HamiltonianParameters<double> HBB(mol_ham_par.get_ao_basis(), ham_par.get_S(), h_b, g_b, SquareMatrix<double>::Identity(K, K));
+            HamiltonianParameters<double> HAB(mol_ham_par.get_ao_basis(), ham_par.get_S(), ham_par.get_h()-h_a-h_b, 2*g_ab, SquareMatrix<double>::Identity(K, K));
+            HamiltonianParameters<double> HA(mol_ham_par.get_ao_basis(), ham_par.get_S(), (ham_par.get_h()-h_a-h_b)/2, g_ab, SquareMatrix<double>::Identity(K, K));
             //HamiltonianParameters<double> HA();
             //HamiltonianParameters<double> HB;
     };
