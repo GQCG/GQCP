7cf3cfe59227fac8d53b7641ac7c34f7cf06f82b
tmhuysen <tobias.huysentruyt@minimoa.net>
2019-04-30 15:05:04 +0200

    added marios thesis decomposition

--- include/HamiltonianParameters/AtomicDecompositionParameters.hpp
+++ include/HamiltonianParameters/AtomicDecompositionParameters.hpp
@@ -32,15 +32,38 @@ namespace GQCP {
  */
 struct AtomicDecompositionParameters {
 
-    Molecule molecule;  // Decomposed molecule
-
     HamiltonianParameters<double> molecular_hamiltonian_parameters;  // the Hamiltonian Parameters of the complete molecule
     std::vector<HamiltonianParameters<double>> net_atomic_parameters;  // vector of net atomic Hamiltonian parameters, E_AA, E_BB
     std::vector<HamiltonianParameters<double>> interaction_parameters;  // vector of interaction Hamiltonian parameters, E_AB
     std::vector<HamiltonianParameters<double>> fragment_parameters;  // vector of total atomic or fragment contributions, E_AA + E_AB/2, E_BB + E_AB/2
 
 
-    explicit AtomicDecompositionParameters (const Molecule& molecule, const std::string& basisset_name) : molecule(molecule), molecular_hamiltonian_parameters(HamiltonianParameters<double>::Molecular(molecule, basisset_name)) {
+    AtomicDecompositionParameters (const HamiltonianParameters<double>& ham_par, const std::vector<HamiltonianParameters<double>>& net_atomic_parameters,
+            const std::vector<HamiltonianParameters<double>>& interaction_parameters, const std::vector<HamiltonianParameters<double>>& fragment_parameters) :
+    molecular_hamiltonian_parameters(ham_par),
+    net_atomic_parameters(net_atomic_parameters),
+    interaction_parameters(interaction_parameters),
+    fragment_parameters(fragment_parameters)
+    {};
+
+    /**
+     *  Constructs net atomic, atomic and atomic interaction Hamiltonian parameters in an AO basis
+     *
+     *  @param molecule     the molecule for which the AtomicDecompositionParameters should be calculated
+     *  @param basisset     the name of the basisset corresponding to the AO basis
+     *
+     *  @return Atomic decomposed parameters:
+     *      - net atomic parameters:
+     *          - one- and two-electron contributions separated only for atomic basis functions centered on an atom.
+     *      - interaction parameters:
+     *          - one- and two-electron contributions for all pairs of atomic basis functions centered on two different atoms
+     *          - scalar : nuclear repulsion
+     *      - atomic fragment parameters:
+     *          - net atomic parameters + interaction parameters/2
+     *
+     *  Note that this named constructor is only available for real matrix representations
+     */
+     AtomicDecompositionParameters (const Molecule& molecule, const std::string& basisset_name) : molecular_hamiltonian_parameters(HamiltonianParameters<double>::Molecular(molecule, basisset_name)) {
 
         auto atoms = molecule.get_atoms();
 
@@ -96,6 +119,77 @@ struct AtomicDecompositionParameters {
         this->net_atomic_parameters = {HAA, HBB};
         this->interaction_parameters = {HAB};
         this->fragment_parameters = {HA, HB};
+    };
+
+    static AtomicDecompositionParameters Mario(const Molecule& molecule, const std::string& basisset_name) {
+
+        auto atoms = molecule.get_atoms();
+
+        if (atoms.size() > 2) {
+            throw std::invalid_argument("HamiltonianParameters::atomicDecomposition(): The Hamiltonian parameters are set up for more than 2 atoms, currently only available for diatomic molecules");
+        }
+
+        Molecule atom_a ({atoms[0]});
+        Molecule atom_b ({atoms[1]});
+
+        auto ao_basis = std::make_shared<AOBasis>(molecule, basisset_name);
+        AOBasis ao_basis_a (atom_a, basisset_name);
+        AOBasis ao_basis_b (atom_b, basisset_name);
+
+        auto Ka = ao_basis_a.numberOfBasisFunctions();
+        auto Kb = ao_basis_b.numberOfBasisFunctions();
+        auto K = ao_basis->numberOfBasisFunctions();
+
+        auto p_a = SquareMatrix<double>::PartitionMatrix(0, Ka, K);
+        auto p_b = SquareMatrix<double>::PartitionMatrix(Ka, Kb, K);
+
+        SquareMatrix<double> identity = SquareMatrix<double>::Identity(K, K);
+
+        const auto& S = ao_basis->calculateOverlapIntegrals();
+        const auto& T = ao_basis->calculateKineticIntegrals();
+        const auto& V = ao_basis->calculateNuclearIntegrals();
+        const auto& g = ao_basis->calculateCoulombRepulsionIntegrals();
+        auto repulsion = molecule.calculateInternuclearRepulsionEnergy();
+
+
+        OneElectronOperator<double> Ta = ao_basis_a.calculateKineticIntegrals();
+        OneElectronOperator<double> Tb = ao_basis_b.calculateKineticIntegrals();
+        OneElectronOperator<double> Va = ao_basis_a.calculateNuclearIntegrals();
+        OneElectronOperator<double> Vb = ao_basis_b.calculateNuclearIntegrals();
+
+        OneElectronOperator<double> H = T + V;
+
+
+        OneElectronOperator<double> h_a = OneElectronOperator<double>::Zero(K,K);
+        OneElectronOperator<double> h_b = OneElectronOperator<double>::Zero(K,K);
+
+        h_a.block(0,0,Ka,Ka) = Ta + Va;
+        h_b.block(Ka,Ka,Kb,Kb) = Tb + Vb;
+
+        OneElectronOperator<double> h_ab = H - h_a - h_b;
+
+        auto g_a = g;
+        auto g_b = g;
+        auto g_ab = g;
+        auto g_ba = g;
+
+        g_a.fourModeMultiplication<double>(p_a, identity, p_a, identity);
+        g_b.fourModeMultiplication<double>(p_b, identity, p_b, identity);
+        g_ab.fourModeMultiplication<double>(p_a, identity, p_b, identity);
+        g_ba.fourModeMultiplication<double>(p_b, identity, p_a, identity);
+        GQCP::TwoElectronOperator<double> g_abba = g_ab.Eigen() + g_ba.Eigen();
+
+        HamiltonianParameters<double> HAA(ao_basis, S, h_a, g_a, identity);
+        HamiltonianParameters<double> HBB(ao_basis, S, h_b, g_b, identity);
+        HamiltonianParameters<double> HAB(ao_basis, S, h_ab, g_abba, identity, repulsion);
+        HamiltonianParameters<double> HA(ao_basis, S, h_a + h_ab/2, g_a.Eigen() + (0.5)*g_abba.Eigen(), identity);
+        HamiltonianParameters<double> HB(ao_basis, S, h_b + h_ab/2, g_b.Eigen() + (0.5)*g_abba.Eigen(), identity);
+
+        std::vector<HamiltonianParameters<double>> net_atomic_parameters = {HAA, HBB};
+        std::vector<HamiltonianParameters<double>> interaction_parameters = {HAB};
+        std::vector<HamiltonianParameters<double>> fragment_parameters = {HA, HB};
+
+        return AtomicDecompositionParameters(HamiltonianParameters<double>::Molecular(molecule, basisset_name), net_atomic_parameters, interaction_parameters, fragment_parameters);
     };
 };
 
--- tests/properties/atomic_decomposition_test.cpp
+++ tests/properties/atomic_decomposition_test.cpp
@@ -89,3 +89,65 @@ BOOST_AUTO_TEST_CASE ( decomposition_BeH_cation_STO_3G ) {
     BOOST_CHECK(std::abs(self_energy_b + interaction_energy_ab / 2 - total_energy_b - repulsion / 2) < 1.0e-010);
 
 }
+
+
+
+BOOST_AUTO_TEST_CASE ( decomposition_BeH_cation_STO_3G_Mario ) {
+
+    // Create the molecular Hamiltonian parameters in an AO basis
+    GQCP::Atom Be(4, 0.0, 0.0, 0.0);
+    GQCP::Atom H(1, 0.0, 0.0, GQCP::units::angstrom_to_bohr(1.134));  // from CCCBDB, STO-3G geometry
+    std::vector<GQCP::Atom> atoms{Be, H};
+    GQCP::Molecule BeH(atoms, +1);
+    GQCP::AtomicDecompositionParameters adp = GQCP::AtomicDecompositionParameters::Mario(BeH, "STO-3G");
+    auto mol_ham_par = adp.molecular_hamiltonian_parameters;
+    auto K = mol_ham_par.get_K();
+    double repulsion = BeH.calculateInternuclearRepulsionEnergy();
+    // Create a plain RHF SCF solver and solve the SCF equations
+    GQCP::PlainRHFSCFSolver plain_scf_solver(mol_ham_par, BeH);
+    plain_scf_solver.solve();
+    auto rhf = plain_scf_solver.get_solution();
+
+    const auto &T = rhf.get_C();
+
+    // Transform the ham_par
+    mol_ham_par.transform(T);
+
+    // Create the FCI module
+    GQCP::ProductFockSpace fock_space(K, BeH.get_N() / 2, BeH.get_N() / 2);  // dim = 441
+    GQCP::FCI fci(fock_space);
+    GQCP::CISolver ci_solver(fci, mol_ham_par);
+
+    // Solve Davidson
+    GQCP::DavidsonSolverOptions solver_options(fock_space.HartreeFockExpansion());
+    ci_solver.solve(solver_options);
+
+    // Retrieve the eigenpair
+    auto fci_coeff = ci_solver.get_eigenpair().get_eigenvector();
+    auto fci_energy = ci_solver.get_eigenpair().get_eigenvalue();
+
+    // Decomposition calculations require RDM-tracing
+    GQCP::RDMCalculator rdm_calc(fock_space);
+    rdm_calc.set_coefficients(fci_coeff);
+
+    auto one_rdm = rdm_calc.calculate1RDMs().one_rdm;
+    auto two_rdm = rdm_calc.calculate2RDMs().two_rdm;
+
+    // Transform rdms to the AObasis
+    GQCP::OneRDM<double> ao_one_rdm = T * (one_rdm) * T.adjoint();
+    GQCP::TwoRDM<double> ao_two_rdm = two_rdm;
+    ao_two_rdm.fourModeMultiplication<double>(T.adjoint().conjugate(), T.adjoint(), T.adjoint().conjugate(), T.adjoint());
+
+    double self_energy_a = GQCP::calculateExpectationValue(adp.net_atomic_parameters[0], ao_one_rdm, ao_two_rdm);
+    double self_energy_b = GQCP::calculateExpectationValue(adp.net_atomic_parameters[1], ao_one_rdm, ao_two_rdm);
+    double interaction_energy_ab = GQCP::calculateExpectationValue(adp.interaction_parameters[0], ao_one_rdm,
+                                                                   ao_two_rdm);
+    double total_energy_a = GQCP::calculateExpectationValue(adp.fragment_parameters[0], ao_one_rdm, ao_two_rdm);
+    double total_energy_b = GQCP::calculateExpectationValue(adp.fragment_parameters[1], ao_one_rdm, ao_two_rdm);
+
+    BOOST_CHECK(std::abs(total_energy_a + total_energy_b - fci_energy) < 1.0e-010);
+    BOOST_CHECK(std::abs(self_energy_a + self_energy_b + interaction_energy_ab - fci_energy - repulsion) < 1.0e-010);
+    BOOST_CHECK(std::abs(self_energy_a + interaction_energy_ab / 2 - total_energy_a - repulsion / 2) < 1.0e-010);
+    BOOST_CHECK(std::abs(self_energy_b + interaction_energy_ab / 2 - total_energy_b - repulsion / 2) < 1.0e-010);
+
+}
