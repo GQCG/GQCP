be4cc48bb35df968d22e1a4d2dc2290caf861d9d
Laurent Lemmens <laurent_lemmens@hotmail.com>
2019-06-21 11:02:05 +0200

    Implement CI electrical response equations

---
+++ include/Properties/BaseElectricalResponseSolver.hpp
@@ -0,0 +1,74 @@
+// This file is part of GQCG-gqcp.
+// 
+// Copyright (C) 2017-2019  the GQCG developers
+// 
+// GQCG-gqcp is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// GQCG-gqcp is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with GQCG-gqcp.  If not, see <http://www.gnu.org/licenses/>.
+// 
+#ifndef GQCP_BASEELECTRICALRESPONSESOLVER_HPP
+#define GQCP_BASEELECTRICALRESPONSESOLVER_HPP
+
+
+#include "HamiltonianParameters/HamiltonianParameters.hpp"
+#include "math/Matrix.hpp"
+#include "Operator/OneElectronOperator.hpp"
+
+#include <array>
+
+
+
+namespace GQCP {
+
+
+class BaseElectricalResponseSolver {
+public:
+    // DESTRUCTOR
+    virtual ~BaseElectricalResponseSolver() = default;
+
+
+    // PUBLIC PURE VIRTUAL METHODS
+
+    /**
+     *  @param ham_par                  the Hamiltonian parameters
+     * 
+     *  @return the parameter response constant (k_p), i.e. the second-order parameter partial derivative of the energy/Lagrangian
+     */
+    virtual SquareMatrix<double> calculateParameterResponseConstant(const HamiltonianParameters<double>& ham_par) = 0;
+
+    /**
+     *  @param dipole_integrals         the dipole integrals in an orthonormal orbital basis
+     * 
+     *  @return the parameter response force (F_p), i.e. the first-order parameter partial derivative of the perturbation derivative of the energy/Lagrangian
+     */
+    virtual Matrix<double, Dynamic, 3> calculateParameterResponseForce(const std::array<OneElectronOperator<double>, 3>& dipole_integrals) = 0;
+
+
+    // PUBLIC METHODS
+
+    /**
+     *  Solve the linear response equations for the wave function response
+     * 
+     *  @param ham_par                  the Hamiltonian parameters
+     *  @param dipole_integrals         the dipole integrals in an orthonormal orbital basis
+     * 
+     *  @return the wave function response
+     */
+    VectorX<double> calculateWaveFunctionResponse(const HamiltonianParameters<double>& ham_par, const std::array<OneElectronOperator<double>, 3>& dipole_integrals);
+};
+
+
+}  // namespace GQCP
+
+
+
+#endif  // GQCP_BASEELECTRICALRESPONSESOLVER_HPP
---
+++ src/Properties/DOCIElectricalResponseSolver.cpp
@@ -0,0 +1,70 @@
+#include "Properties/CIElectricalResponseSolver.hpp"
+
+
+namespace GQCP {
+
+
+/*
+ *  CONSTRUCTORS
+ */
+
+/**
+ *  @param wf           the CI wave function
+ *  @param molecule                 the molecule
+ */
+CIElectricalResponseSolver::CIElectricalResponseSolver(const WaveFunction& wave_function, const Molecule& molecule) :
+    wave_function (wave_function),
+    molecule (molecule)
+{}
+
+
+
+/*
+ *  PUBLIC OVERRIDDEN METHODS
+ */
+
+/**
+ *  @param ham_par                  the Hamiltonian parameters
+ * 
+ *  @return the parameter response constant (k_p), i.e. the second-order parameter partial derivative of the CI energy
+ */
+SquareMatrix<double> CIElectricalResponseSolver::calculateParameterResponseConstant(const HamiltonianParameters<double>& ham_par) {
+
+    // k_p for DOCI is just the Hamiltonian evaluated in the Fock space
+    return 2 * this->wave_function.get_fock_space().evaluateOperatorDense(ham_par, true);  // true: need to calculate diagonal values as well
+}
+
+
+/**
+ *  @param dipole_integrals         the dipole integrals in an orthonormal orbital basis
+ * 
+ *  @return the parameter response force (F_p), i.e. the first-order parameter partial derivative of the perturbation derivative of the energy
+ */
+Matrix<double, Dynamic, 3> CIElectricalResponseSolver::calculateParameterResponseForce(const std::array<OneElectronOperator<double>, 3>& dipole_integrals) {
+
+    const auto dim = this->wave_function.get_fock_space().get_dimension();  // dimension of the Fock space
+    Matrix<double, Dynamic, 3> F_p = Matrix<double, Dynamic, 3>::Zero(dim, 3);
+
+
+    // F_p for DOCI is a matrix-vector product of the total dipole operator and the DOCI wave function
+    const auto nuclear_dipole_moment = this->molecule.calculateNuclearDipoleMoment();
+    for (size_t m = 0; m < 3; m++) {  // m loops over the components of the electrical dipole
+
+        // Generate the total dipole operator matrix representation
+        auto mu_m = this->wave_function.get_fock_space().evaluateOperatorDense(dipole_integrals[m], true);  // true: need to calculate diagonal values as well
+
+        // Set the nuclear contributions on the diagonal
+        for (size_t J = 0; J < dim; J++) {
+            mu_m(J,J) += nuclear_dipole_moment(m);
+        }
+
+        // Do the matvec explicitly and set the result as a column
+        F_p.col(m) = mu_m * this->wave_function.get_coefficients();
+    }
+
+
+    return -2 * F_p;
+}
+
+
+}  // namespace GQCP
