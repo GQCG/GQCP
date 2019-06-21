92a5b1e6306e133c6eb2a00d8dd5122c79d178d7
guacke <Guillaume.Acke@UGent.be>
2019-06-21 21:13:11 +0200

    Switch to PyBind11 due to unstable nature of Boost.Python.

--- src/bindings/bindings.cpp
+++ src/bindings/bindings.cpp
@@ -1,9 +1,23 @@
-#include <boost/python.hpp>
-#include <boost/python/numpy.hpp>
+#include <pybind11/eigen.h>
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
 
-namespace np = boost::python::numpy;
+#include "Atom.hpp"
+#include "HamiltonianParameters/HamiltonianParameters.hpp"
+#include "Molecule.hpp"
+#include "typedefs.hpp"
 
-BOOST_PYTHON_MODULE(pygqcp) {
-    Py_Initialize();
-    np::initialize();
+namespace py = pybind11;
+
+PYBIND11_MODULE(gqcpy, m) {
+    py::class_<GQCP::Atom>(m, "Atom")
+            .def(py::init<int, double, double, double>())
+            ;
+    py::class_<GQCP::Molecule>(m, "Molecule")
+            .def(py::init<std::vector<GQCP::Atom>, int>())
+            .def("read_xyz", &GQCP::Molecule::Readxyz)
+            .def("get_N", &GQCP::Molecule::get_N)
+            .def("get_atoms", &GQCP::Molecule::get_atoms)
+            .def("number_of_atoms", &GQCP::Molecule::numberOfAtoms)
+            ;
 }
