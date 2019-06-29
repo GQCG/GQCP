9f337ad36ade408de602a276c3f896164a00ba26
guacke <Guillaume.Acke@UGent.be>
2019-06-29 15:35:37 +0200

    Hubbard Driver python binding.

---
+++ include/Drivers/HubbardDriver.hpp
@@ -0,0 +1,45 @@
+/**
+ *  An executable that calculates the Hubbard energy/energies, starting from a triagonal input specifying the hopping matrix. The energy/energies are printed to the console.
+ *
+ *  Example execution:
+ *      ./hubbard -m "comma-separated upper triagonal" -a "number of alpha electrons" -b "number of beta electrons" -K "number of sites"
+ *
+ *  For example:
+ *      ./hubbard -m "1,2,3,4,5,6" -a 1 -b 2 -K 3
+ *  Default amount of eigenvalues is 1, this can be changed with the x flag:
+ *      ./hubbard -m "1,2,3,4,5,6" -x 2 -a 1 -b 2 -K 3
+ */
+
+
+
+#include <cstddef>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <string>
+
+#include <boost/algorithm/string.hpp>
+#include <boost/program_options.hpp>
+
+#include "HamiltonianParameters/HamiltonianParameters.hpp"
+#include "CISolver/CISolver.hpp"
+#include "HamiltonianBuilder/Hubbard.hpp"
+
+namespace GQCP {
+    class HubbardDriver {
+    private:
+        std::string csline;
+        size_t num_states;
+        size_t num_alpha;
+        size_t num_beta;
+        size_t K;
+        std::vector<double> energies;
+
+    public:
+        HubbardDriver(std::string csline, size_t num_states, size_t num_alpha, size_t num_beta, size_t K);
+
+        std::vector<double> get_energies() {
+            return this->energies;
+        }
+    };
+}
