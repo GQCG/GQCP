f486149f00e2cced5cd76793f553e78d6c341813
Laurent Lemmens <laurent_lemmens@hotmail.com>
2019-07-03 22:42:37 +0200

    Define basic engine classes

---
+++ include/Basis/Libint2OneElectronIntegralEngine.hpp
@@ -0,0 +1,40 @@
+// This file is part of GQCG-gqcp.
+// 
+// Copyright (C) 2017-2019  the GQCG developers
+// 
+// GQCG-gqcp is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// GQCG-gqcp is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with GQCG-gqcp.  If not, see <http://www.gnu.org/licenses/>.
+// 
+#ifndef GQCP_LIBINT2ONEELECTRONINTEGRALENGINE_HPP
+#define GQCP_LIBINT2ONEELECTRONINTEGRALENGINE_HPP
+
+
+#include "Basis/BaseOneElectronIntegralEngine.hpp"
+
+
+namespace GQCP {
+
+
+/**
+ *  A one-electron integral engine that uses Libint2 as its backend
+ */
+template <size_t _N, typename _Scalar>
+class Libint2OneElectronIntegralEngine {
+};
+
+
+}  // namespace GQCP
+
+
+
+#endif  // GQCP_LIBINT2ONEELECTRONINTEGRALENGINE_HPP
