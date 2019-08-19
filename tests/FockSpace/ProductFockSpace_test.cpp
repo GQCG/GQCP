46cbf7d5d04d7e4049854f53e0d8f763332c5d8c
tmhuysen <tobias.huysentruyt@minimoa.net>
2019-08-20 00:00:04 +0200

    updated test documentation fock space evaluations

--- tests/FockSpace/ProductFockSpace_test.cpp
+++ tests/FockSpace/ProductFockSpace_test.cpp
@@ -58,6 +58,7 @@ BOOST_AUTO_TEST_CASE ( FockSpace_EvaluateOperator_Dense_diagonal_true ) {
     auto& h = parameters.get_h();
     auto& g = parameters.get_g();
 
+    // Test the evaluation of the operators with selected Fock space (the reference) versus the that of the product Fock space 
     auto one_electron_evaluation1 = product_fock_space.evaluateOperatorDense(h, true);
     auto one_electron_evaluation2 = selected_fock_space.evaluateOperatorDense(h, true);
 
@@ -85,6 +86,7 @@ BOOST_AUTO_TEST_CASE ( FockSpace_EvaluateOperator_Dense_diagonal_false ) {
     auto& h = parameters.get_h();
     auto& g = parameters.get_g();
 
+    // Test the evaluation of the operators with selected Fock space (the reference) versus the that of the product Fock space 
     auto one_electron_evaluation1 = product_fock_space.evaluateOperatorDense(h, false);
     auto one_electron_evaluation2 = selected_fock_space.evaluateOperatorDense(h, false);
 
@@ -112,6 +114,7 @@ BOOST_AUTO_TEST_CASE ( FockSpace_EvaluateOperator_diagonal ) {
     auto& h = parameters.get_h();
     auto& g = parameters.get_g();
 
+    // Test the evaluation of the operators with selected Fock space (the reference) versus the that of the product Fock space 
     auto one_electron_evaluation1 = product_fock_space.evaluateOperatorDiagonal(h);
     auto one_electron_evaluation2 = selected_fock_space.evaluateOperatorDiagonal(h);
 
