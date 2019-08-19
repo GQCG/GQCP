e60b4e0cb1ad66a344adaecdcc11deecf1db22bf
tmhuysen <tobias.huysentruyt@minimoa.net>
2019-08-19 17:19:09 +0200

    removed drivers, extended python tests

---
+++ gqcpy/tests/fci_binding_test.py
@@ -0,0 +1,27 @@
+import unittest
+import numpy as np
+
+# Force the local gqcpy to be imported
+import sys
+sys.path.insert(0, '../')
+
+import gqcpy
+
+class FCIQCM(unittest.TestCase):
+
+    def setUp(self):
+        """ Iniates variables to be used by tests """
+        self.fci_module = gqcpy.FCI("data/h2o_Psi4_GAMESS.xyz", "STO-3G", 5, 5)
+        self.ref_energy = -75.0129803939602
+        self.fci_module.solve()
+
+    def tearDown(self):
+        pass
+
+    def test_energies(self):
+        """ Compare the energy with a reference value """
+        self.assertAlmostEqual( self.fci_module.get_energy(), self.ref_energy)
+
+    
+if __name__ == '__main__':
+    unittest.main()
---
+++ gqcpy/tests/oo-doci_binding_test.py
@@ -0,0 +1,28 @@
+import unittest
+import numpy as np
+
+# Force the local gqcpy to be imported
+import sys
+sys.path.insert(0, '../')
+
+import gqcpy
+
+
+class HubbardQCM(unittest.TestCase):
+
+    def setUp(self):
+        """ Iniates variables to be used by tests """
+        self.oo_doci_module = gqcpy.DOCINewtonOrbitalOptimizer("data/h2_cristina.xyz", "STO-3G", False, False)
+        double reference_fci_energy = -1.13726333769813
+        self.oo_doci_module.solve()
+
+    def tearDown(self):
+        pass
+
+    def test_energies(self):
+        """ Compare the energy with a reference value """
+        self.assertAlmostEqual( self.oo_doci_module.get_energy(), self.ref_energy)
+
+    
+if __name__ == '__main__':
+    unittest.main()
