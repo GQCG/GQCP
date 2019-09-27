076ff5ac732f9092c298547d3ea73e2709d0333c
tmhuysen <tobias.huysentruyt@minimoa.net>
2019-09-27 11:47:44 +0200

    added fukui dyson binding

--- include/QCMethod/FukuiDysonAnalysis.hpp
+++ include/QCMethod/FukuiDysonAnalysis.hpp
@@ -17,12 +17,10 @@
 //
 #pragma once
 
-
+#include "Basis/SingleParticleBasis.hpp"
 #include "CISolver/CISolver.hpp"
 #include "FockSpace/ProductFockSpace.hpp"
 #include "HamiltonianBuilder/FCI.hpp"
-#include "HamiltonianBuilder/FrozenCoreFCI.hpp"
-#include "HamiltonianParameters/AtomicDecompositionParameters.hpp"
 #include "HamiltonianParameters/HamiltonianParameters.hpp"
 #include "RDM/RDMCalculator.hpp"
 
@@ -32,62 +30,23 @@ namespace QCMethod {
 
 
 /**
- *  A class that solves the FCI Hamiltonian for a given singlet molecule and performs Fukui and Dyson analysis
- *   Restricted Hartree-Fock is initially performed for the singlet, 
- *   after which we solve in the FCI space and solve in the FCI space again after removing an electron
+ *  A class that solves the FCI Hamiltonian for a given molecule and performs Fukui and Dyson analysis
+ *  restricted Hartree-Fock is initially performed for the singlet, 
+ *  after which we solve in the FCI space and solve in the FCI space again after removing an electron
  */
 class FukuiDysonAnalysis {
 private:
-    double solve_time;
     Molecule molecule;
-    HamiltonianParameters<double> ham_par;
+    SingleParticleBasis<double, GTOShell> sp_basis;
+    SQHamiltonian<double> sq_hamiltonian;
     std::string basis_set;  // the basisset that should be used
-    FrozenProductFockSpace fock_space = FrozenProductFockSpace(0, 0, 0, 0); // Default
-    FrozenCoreFCI fci = FrozenCoreFCI(FrozenProductFockSpace(0, 0, 0, 0)); 
-    ScalarSQOneElectronOperator<double> mulliken_operator;
-    AtomicDecompositionParameters adp = AtomicDecompositionParameters();
-    RDMCalculator rdm_calculator = RDMCalculator();
+    ProductFockSpace fock_space1 = ProductFockSpace(0, 0, 0, 0); // Default
+    ProductFockSpace fock_space2 = ProductFockSpace(0, 0, 0, 0); // Default
 
-    bool are_solutions_available = false;
-
-    // Molecular solutions
-    std::vector<double> energy;
-    std::vector<double> population; 
-    std::vector<double> lambda;
-    std::vector<double> entropy;
-
-    // Decomposed solutions
-    std::vector<double> A_fragment_energy;
-    std::vector<double> A_fragment_self_energy;
-    std::vector<double> B_fragment_energy;
-    std::vector<double> B_fragment_self_energy;
-    std::vector<double> interaction_energy;
-
-    // Eigenvectors
-    std::vector<VectorX<double>> eigenvector;
-
-    // PRIVATE METHODS
-    /**
-     *  Store the solutions from a solve
-     *  
-     *  @param eigenpairs           the eigenpairs from the CI solver
-     *  @param multiplier           the Lagrangian multiplier associated with the solution
-     */ 
-    void parseSolution(const std::vector<Eigenpair>& eigenpairs, const double multiplier);
-
-    /**
-     *  Throws an error if no solution is available
-     *  
-     *  @param function_name            name of the function that should throw the error
-     */
-    void checkAvailableSolutions(const std::string& function_name) const;
-
-    /**
-     *  Throws an error if the molecule is not diatomic
-     *  
-     *  @param function_name            name of the function that should throw the error
-     */
-    void checkDiatomicMolecule(const std::string& function_name) const;
+    VectorX<double> dyson_coefficients;
+    OneRDM<double> fukui_matrix;
+    OneRDM<double> fukui_naturals;
+    SquareMatrix<double> fukui_vectors;
 
 public:
 
@@ -95,64 +54,17 @@ public:
     /**
      *  @param molecule                 the molecule that will be solved for
      *  @param basis_set                the basisset that should be used
-     *  @param basis_targets            the targeted basis functions for the constraint
-     *  @param frozencores              the amount of frozen cores for the FCI calculation
+     *  @param use_diis                 flags if one wants to use the DIIS for the RHF solve as opposed to Plain RHF
      */
-    FukuiDysonAnalysis(const Molecule& molecule, const std::string& basis_set);
+    FukuiDysonAnalysis(const Molecule& molecule, const std::string& basis_set, const bool use_diis);
 
 
-    // PUBLIC METHODS
-    /**
-     *  Solve the eigenvalue problem for a multiplier with the davidson algorithm
-     *  
-     *  @param multiplier           a given multiplier    
-     *  @param guess                supply a davidson guess         
-     */
-    void solveMullikenDavidson(const double multiplier, const VectorX<double>& guess);
-
-    /**
-     *  Solve the eigenvalue problem for a multiplier with the davidson algorithm, davidson guess will be the previously stored solution
-     *  if none is available the Hartree Fock expansion will be used instead
-     *  
-     *  @param multiplier           a given multiplier          
-     */
-    void solveMullikenDavidson(const double multiplier);
-
-    /**
-     *  Solve the eigenvalue problem for a the next multiplier dense
-     * 
-     *  @param multiplier           a given multiplier       
-     *  @param nos                  the number of eigenpairs or "states" that should be stored for each multiplier
-     */
-    void solveMullikenDense(const double multiplier, const size_t nos);
-
-    /**
-     *  @param index                refers to the index of the number of requested states 
-     * 
-     *  @return a property of the last solve
-     */
-    double get_energy(const size_t index = 0) const { this->checkAvailableSolutions("get_energy"); return this->energy[index]; };
-    double get_population(const size_t index = 0) const { this->checkAvailableSolutions("get_population"); return this->population[index]; };
-    double get_lambda(const size_t index = 0) const { this->checkAvailableSolutions("get_lambda"); return this->lambda[index]; };
-    double get_entropy(const size_t index = 0) const { this->checkAvailableSolutions("get_entropy"); return this->entropy[index]; };
-    double get_A_fragment_energy(const size_t index = 0) const { this->checkAvailableSolutions("get_A_fragment_energy"); this->checkDiatomicMolecule("get_A_fragment_energy"); return this->A_fragment_energy[index]; };
-    double get_A_fragment_self_energy(const size_t index = 0) const { this->checkAvailableSolutions("get_A_fragment_self_energy"); this->checkDiatomicMolecule("get_A_fragment_self_energy"); return this->A_fragment_self_energy[index]; };
-    double get_B_fragment_energy(const size_t index = 0) const { this->checkAvailableSolutions("get_B_fragment_energy"); this->checkDiatomicMolecule("get_B_fragment_energy"); return this->B_fragment_energy[index]; };
-    double get_B_fragment_self_energy(const size_t index = 0) const { this->checkAvailableSolutions("get_B_fragment_self_energy"); this->checkDiatomicMolecule("get_B_fragment_self_energy"); return this->B_fragment_self_energy[index]; };
-    double get_interaction_energy(const size_t index = 0) const { this->checkAvailableSolutions("get_interaction_energy"); this->checkDiatomicMolecule("get_interaction_energy"); return this->interaction_energy[index]; };
-
-    const VectorX<double>& get_eigenvector(const size_t index = 0) const { this->checkAvailableSolutions("get_eigenvector"); return this->eigenvector[index]; };
-
-    double get_solve_time() const { this->checkAvailableSolutions("get_solve_time"); return solve_time; };
-    
-    /**
-     *  @param index             refers to the index of the number of requested states 
-     * 
-     *  @return all properties in vector that contains:
-     *      energy, population (on the selected basis functions), lambda (or the multiplier), entropy
-     *      if diatomic we additionally find: A_fragment_energy, A_fragment_self_energy, B_fragment_energy, B_fragment_self_energy and interaction_energy in that order.
-     */
-    std::vector<double> all_properties(const size_t index = 0) const; 
+    // GETTERS
+    VectorX<double> get_dyson_coefficients(const size_t index = 0) const { this->dyson_coefficients; };
+    OneRDM<double> get_fukui_matrix(const size_t index = 0) const { this->fukui_matrix; };
+    OneRDM<double> get_fukui_naturals(const size_t index = 0) const { this->fukui_naturals; };
+    SquareMatrix<double> get_fukui_vectors(const size_t index = 0) const { this->fukui_vectors; };
+    SquareMatrix<double> get_canonical_matrix(const size_t index = 0) const { this->sp_basis.transformationMatrix(); };
 };
 
 
---
+++ src/QCMethod/FukuiDysonAnalysis.cpp
@@ -0,0 +1,115 @@
+// This file is part of GQCG-gqcp.
+//
+// Copyright (C) 2017-2019  the GQCG developers
+//
+// GQCG-gqcp is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// GQCG-gqcp is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with GQCG-gqcp.  If not, see <http://www.gnu.org/licenses/>.
+//
+#include "QCMethod/FukuiDysonAnalysis.hpp"
+
+#include "Basis/transform.hpp"
+#include "RHF/DIISRHFSCFSolver.hpp"
+#include "RHF/PlainRHFSCFSolver.hpp"
+
+
+namespace GQCP {
+namespace QCMethod {
+
+
+/*
+ * CONSTRUCTORS
+ */
+
+/**
+ *  @param molecule                 the molecule that will be solved for
+ *  @param basis_set                the basisset that should be used
+ *  @param use_diis                 flags if one wants to use the DIIS for the RHF solve as opposed to Plain RHF
+ */
+FukuiDysonAnalysis::FukuiDysonAnalysis(const Molecule& molecule, const std::string& basis_set, const bool use_diis) : 
+        molecule (molecule),
+        sp_basis (SingleParticleBasis<double, GTOShell>(molecule, basis_set)),
+        sq_hamiltonian (SQHamiltonian<double>::Molecular(this->sp_basis, molecule)),  // in AO basis
+        basis_set (basis_set)
+{
+    const auto K = this->sp_basis.numberOfBasisFunctions();
+    const auto N_P = this->molecule.numberOfElectrons()/2;
+
+    auto restricted_molecule = this->molecule; 
+    const bool open_shell_entry = N_P*2 != this->molecule.numberOfElectrons();
+    if (open_shell_entry) {
+        restricted_molecule = Molecule(this->molecule.nuclearFramework(), +1);
+    }
+
+    if (use_diis) {
+        DIISRHFSCFSolver diis_scf_solver (this->sq_hamiltonian, this->sp_basis, restricted_molecule, 6, 6, 1e-12, 500);
+        diis_scf_solver.solve();
+        auto rhf_solution = diis_scf_solver.get_solution();
+        basisTransform(this->sp_basis, this->sq_hamiltonian, rhf_solution.get_C());
+    } else {
+        PlainRHFSCFSolver plain_scf_solver (this->sq_hamiltonian, this->sp_basis, restricted_molecule, 6, 6, 1e-12, 500);
+        plain_scf_solver.solve();
+        auto rhf_solution = plain_scf_solver.get_solution();
+        basisTransform(this->sp_basis, this->sq_hamiltonian, rhf_solution.get_C());
+    }
+
+    if (open_shell_entry) {
+        this->fock_space1 = ProductFockSpace(K, N_P + 1, N_P);
+        this->fock_space2 = ProductFockSpace(K, N_P, N_P);
+    } else {
+        this->fock_space1 = ProductFockSpace(K, N_P, N_P);
+        this->fock_space2 = ProductFockSpace(K, N_P, N_P - 1); 
+    }
+
+    FCI fci1 = FCI(fock_space1);
+    FCI fci2 = FCI(fock_space2);
+    
+    CISolver ci_solver1(fci1, sq_hamiltonian);
+    CISolver ci_solver2(fci2, sq_hamiltonian);
+
+    // Solve Davidson
+    VectorX<double> initial_g1 = fock_space1.HartreeFockExpansion();
+    VectorX<double> initial_g2 = fock_space2.HartreeFockExpansion();
+    DavidsonSolverOptions davidson_solver_options1 (initial_g1);
+    DavidsonSolverOptions davidson_solver_options2 (initial_g2);
+    ci_solver1.solve(davidson_solver_options1);
+    ci_solver2.solve(davidson_solver_options2);
+
+    const auto wavefunction1 = ci_solver1.makeWavefunction();
+    const auto wavefunction2 = ci_solver2.makeWavefunction();
+
+    this->dyson_coefficients = GQCP::calculateDysonAmplitudes(wavefunction1, wavefunction2);
+    
+    RDMCalculator rdm_calculator1 (fock_space1);
+
+    rdm_calculator1.set_coefficients(wavefunction1.get_coefficients());
+
+    RDMCalculator rdm_calculator2 (fock_space2);
+
+    rdm_calculator2.set_coefficients(wavefunction2.get_coefficients());
+
+    const auto onerdm1 = rdm_calculator1.calculate1RDMs().one_rdm;
+    const auto onerdm2 = rdm_calculator2.calculate1RDMs().one_rdm;
+
+    this->fukui_matrix = onerdm1 - onerdm2;
+
+    Eigen::SelfAdjointEigenSolver<Eigen::MatrixXd> saes1 (this->fukui_matrix);
+
+    this->fukui_naturals = OneRDM<double>(saes1.eigenvalues().asDiagonal());
+    this->fukui_vectors = OneRDM<double>(saes1.eigenvectors());
+}
+
+
+}  // namespace QCMethod
+}  // namespace GQCP
+
+
