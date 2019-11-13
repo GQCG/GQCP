6053d5db8334f3278affd740b2cd1388fd81f387
tmhuysen <tobias.huysentruyt@minimoa.net>
2019-11-13 10:44:44 +0100

    added the USQHamiltonian

---
+++ include/Operator/SecondQuantized/USQHamiltonian.hpp
@@ -0,0 +1,250 @@
+// This file is part of GQCG-gqcp.
+// 
+// Copyright (C) 2017-2019  the GQCG developers
+// 
+// GQCG-gqcp is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// GQCG-gqcp is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with GQCG-gqcp.  If not, see <http://www.gnu.org/licenses/>.
+// 
+#pragma once
+
+
+#include "Operator/SecondQuantized/SQHamiltonian.hpp"
+
+
+namespace GQCP {
+
+
+/**
+ *  A class for representing the unrestricted second-quantized electronic Hamiltonian
+ *  It consists of two restricted Hamiltonians for both the beta and alpha component and a mixed two-electron term
+ * 
+ *
+ *  @tparam Scalar      the scalar type of the second-quantized parameters (i.e. the integrals)
+ */
+template <typename Scalar>
+class USQHamiltonian {
+private:
+    SQHamiltonian<Scalar> sq_hamiltonian_alpha;  // alpha hamiltonian
+    SQHamiltonian<Scalar> sq_hamiltonian_beta;  // beta hamiltonian
+
+    std::vector<ScalarSQTwoElectronOperator<Scalar>> two_op_mixed;  // alpha & beta mixed operators represented as g_aabb
+    ScalarSQTwoElectronOperator<Scalar> total_two_op_mixed;  // the total alpha & beta mixed operator represented as g_aabb
+
+public:
+
+    /*
+     *  CONSTRUCTORS
+     */
+    USQHamiltonian() = default;
+
+    /**
+     *  @param sq_hamiltonian_alpha      alpha hamiltonian
+     *  @param sq_hamiltonian_beta       beta hamiltonian
+     *  @param two_op_mixed              alpha & beta mixed operators
+     */
+    USQHamiltonian(const SQHamiltonian<Scalar>& sq_hamiltonian_alpha, const SQHamiltonian<Scalar>& sq_hamiltonian_beta, const std::vector<ScalarSQTwoElectronOperator<Scalar>>& two_op_mixed) :
+        sq_hamiltonian_alpha (sq_hamiltonian_alpha),
+        sq_hamiltonian_beta (sq_hamiltonian_beta),
+        two_op_mixed (two_op_mixed)
+    {
+
+        const auto dim = sq_hamiltonian_alpha.dimension();
+
+        if (sq_hamiltonian_beta.dimension() != dim) {
+            throw std::invalid_argument("USQHamiltonian::USQHamiltonian(const SQHamiltonian<Scalar>& sq_hamiltonian_alpha, const SQHamiltonian<Scalar>& sq_hamiltonian_beta, const ScalarSQTwoElectronOperator<Scalar>& two_op_mixed): The dimensions of the alpha and beta Hamiltonian are incompatible");
+        }
+        
+        for (const auto& two_op : this->two_op_mixed) {
+            if (two_op.dimension() != dim) {
+                throw std::invalid_argument("USQHamiltonian::USQHamiltonian(const SQHamiltonian<Scalar>& sq_hamiltonian_alpha, const SQHamiltonian<Scalar>& sq_hamiltonian_beta, const ScalarSQTwoElectronOperator<Scalar>& two_op_mixed): The dimensions of the mixed two electron operator are incompatible with the Hamiltonian");
+            }
+        }
+        
+        // Calculate the total two-electron operator
+        QCRankFourTensor<Scalar> total_two_op_par (dim);
+        total_two_op_par.setZero();
+        for (const auto& two_op : this->two_op_mixed) {
+            total_two_op_par += two_op.parameters().Eigen();
+        }
+        this->total_two_op_mixed = ScalarSQTwoElectronOperator<Scalar>({total_two_op_par});
+    }
+
+    /**
+     *  @param sq_hamiltonian_alpha      alpha hamiltonian
+     *  @param sq_hamiltonian_beta       beta hamiltonian
+     *  @param two_op_mixed              alpha & beta mixed operator
+     */
+    USQHamiltonian(const SQHamiltonian<Scalar>& sq_hamiltonian_alpha, const SQHamiltonian<Scalar>& sq_hamiltonian_beta, const ScalarSQTwoElectronOperator<Scalar>& two_op_mixed) :
+        USQHamiltonian(sq_hamiltonian_alpha, sq_hamiltonian_beta, std::vector<ScalarSQTwoElectronOperator<Scalar>>({two_op_mixed}))
+    {}
+
+
+    /*
+     *  NAMED CONSTRUCTORS
+     */
+
+    /**
+     *  Construct the molecular Hamiltonian in a given single-particle basis
+     *
+     *  @param sp_basis     the single-particle basis in which the Hamiltonian should be expressed
+     *
+     *  @return a second-quantized molecular Hamiltonian. The molecular Hamiltonian has
+     *      - one-electron contributions:
+     *          - kinetic
+     *          - nuclear attraction
+     *      - two-electron contributions:
+     *          - Coulomb repulsion
+     *
+     *  Note that this named constructor is only available for real matrix representations
+     */
+    template <typename Z = Scalar>
+    static enable_if_t<std::is_same<Z, double>::value, USQHamiltonian<double>> Molecular(const SingleParticleBasis<Z, GTOShell>& sp_basis_alpha, const SingleParticleBasis<Z, GTOShell>& sp_basis_beta, const Molecule& molecule) {
+
+        const SQHamiltonian<Scalar> sq_hamiltonian_alpha = SQHamiltonian<double>::Molecular(sp_basis_alpha, molecule);
+        const SQHamiltonian<Scalar> sq_hamiltonian_beta = SQHamiltonian<double>::Molecular(sp_basis_beta, molecule);
+        const ScalarSQTwoElectronOperator<double> two_op_mixed = sq_hamiltonian_alpha.twoElectron();
+
+        return USQHamiltonian(sq_hamiltonian_alpha, sq_hamiltonian_beta, two_op_mixed);
+    }
+
+    /*
+     *  PUBLIC METHODS
+     */
+
+    /**
+     *  @return the dimension of the Hamiltonian, i.e. the number of spinors in which it is expressed
+     */
+    size_t dimension() const { return this->sq_hamiltonian_alpha.dimension(); }
+
+    /**
+     *  @return the total of the alpha contributions of the unrestricted Hamiltonian
+     */
+    const SQHamiltonian<Scalar>& alphaHamiltonian() const { return this->sq_hamiltonian_alpha; }
+
+    /**
+     *  @return the total of the beta contributions of the unrestricted Hamiltonian
+     */
+    const SQHamiltonian<Scalar>& betaHamiltonian() const { return this->sq_hamiltonian_beta; }
+
+    /**
+     *  @return the total contributions to the mixed alpha & beta two-electron part of the unrestricted Hamiltonian
+     */
+    const ScalarSQTwoElectronOperator<Scalar>& twoElectronMixed() const { return this->total_two_op_mixed; }
+
+    /**
+     *  @return the total contributions to the mixed alpha & beta two-electron part of the unrestricted Hamiltonian
+     */
+    const std::vector<ScalarSQTwoElectronOperator<Scalar>>& twoElectronContributionsMixed() const { return this->two_op_mixed; }
+
+    /**
+     *  In-place transform the matrix representations of Hamiltonian
+     *
+     *  @param T    the transformation matrix between the old and the new orbital basis
+     */
+    void transform(const TransformationMatrix<Scalar>& T) {
+
+        this->sq_hamiltonian_alpha.transform(T);
+        this->sq_hamiltonian_beta.transform(T);
+
+        // Transform the mixed
+        this->total_two_op_mixed.transform(T);
+        for (auto& two_op : this->two_op_mixed) {
+            two_op.transform(T);
+        }
+    }
+
+    /**
+     *  In-place transform the matrix representations of the alpha components of the unrestricted Hamiltonian
+     *
+     *  @param T    the transformation matrix between the old and the new orbital basis
+     */
+    void transformAlpha(const TransformationMatrix<Scalar>& T) {
+
+        this->sq_hamiltonian_alpha.transform(T);
+        // Transform the mixed
+        auto new_two_electron_parameters = this->total_two_op_mixed.parameters();
+        // transform the two electron parameters "g_aabb" to "g_a'a'bb"
+        new_two_electron_parameters.template matrixContraction<Scalar>(T, 0);
+        new_two_electron_parameters.template matrixContraction<Scalar>(T, 1);
+        this->total_two_op_mixed = ScalarSQTwoElectronOperator<Scalar> ({new_two_electron_parameters});
+
+        for (auto& two_op : this->two_op_mixed) {
+            auto new_two_electron_parameters = two_op.parameters();
+            // transform the two electron parameters "g_aabb" to "g_a'a'bb"
+            new_two_electron_parameters.template matrixContraction<Scalar>(T, 0);
+            new_two_electron_parameters.template matrixContraction<Scalar>(T, 1);
+            two_op = ScalarSQTwoElectronOperator<Scalar> ({new_two_electron_parameters});
+        }
+    }
+
+
+    /**
+     *  In-place transform the matrix representations of the beta components of the unrestricted Hamiltonian
+     *
+     *  @param T    the transformation matrix between the old and the new orbital basis
+     */
+    void transformBeta(const TransformationMatrix<Scalar>& T) {
+
+        this->sq_hamiltonian_beta.transform(T);
+        // Transform the mixed
+        auto new_two_electron_parameters = this->total_two_op_mixed.parameters();
+        // transform the two electron parameters "g_aabb" to "g_aab'b'"
+        new_two_electron_parameters.template matrixContraction<Scalar>(T, 2);
+        new_two_electron_parameters.template matrixContraction<Scalar>(T, 3);
+        this->total_two_op_mixed = ScalarSQTwoElectronOperator<Scalar> ({new_two_electron_parameters});
+
+        for (auto& two_op : this->two_op_mixed) {
+            auto new_two_electron_parameters = two_op.parameters();
+            // transform the two electron parameters "g_aabb" to "g_aab'b'"
+            new_two_electron_parameters.template matrixContraction<Scalar>(T, 2);
+            new_two_electron_parameters.template matrixContraction<Scalar>(T, 3);
+            two_op = ScalarSQTwoElectronOperator<Scalar> ({new_two_electron_parameters});
+        }
+    }
+
+
+    /**
+     *  Constrain the Hamiltonian according to the convention: - lambda * constraint
+     *
+     *  @param one_op   the one-electron operator used as a constraint
+     *  @param lambda   Lagrangian multiplier for the constraint
+     *
+     *  @return a copy of the constrained Hamiltonian
+     *
+     *  Note that this method is only available for real matrix representations
+     */
+    template<typename Z = Scalar>
+    enable_if_t<std::is_same<Z, double>::value, USQHamiltonian<double>> constrainAlpha(const ScalarSQOneElectronOperator<double>& one_op, double lambda) const {
+
+        return USQHamiltonian(this->sq_hamiltonian_alpha.constrain(one_op, lambda), this->sq_hamiltonian_beta, this->two_op_mixed);
+    }
+
+    /**
+     *  Constrain the Hamiltonian according to the convention: - lambda * constraint
+     *
+     *  @param one_op   the one-electron operator used as a constraint
+     *  @param lambda   Lagrangian multiplier for the constraint
+     *
+     *  @return a copy of the constrained Hamiltonian
+     *
+     *  Note that this method is only available for real matrix representations
+     */
+    template<typename Z = Scalar>
+    enable_if_t<std::is_same<Z, double>::value, USQHamiltonian<double>> constrainBeta(const ScalarSQOneElectronOperator<double>& one_op, double lambda) const {
+
+        return USQHamiltonian(this->sq_hamiltonian_alpha, this->sq_hamiltonian_beta.constrain(one_op, lambda), this->two_op_mixed);
+    }
+};
+
+
+}  // namespace GQCP
---
+++ tests/Operator/SecondQuantized/USQHamiltonian_test.cpp
@@ -0,0 +1,85 @@
+// This file is part of GQCG-gqcp.
+// 
+// Copyright (C) 2017-2019  the GQCG developers
+// 
+// GQCG-gqcp is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// GQCG-gqcp is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with GQCG-gqcp.  If not, see <http://www.gnu.org/licenses/>.
+// 
+#define BOOST_TEST_MODULE "USQHamiltonian"
+
+#include <boost/test/unit_test.hpp>
+
+#include "Operator/SecondQuantized/USQHamiltonian.hpp"
+#include "RHF/PlainRHFSCFSolver.hpp"
+#include "Utilities/miscellaneous.hpp"
+#include "Utilities/linalg.hpp"
+
+#include <boost/math/constants/constants.hpp>
+
+
+
+BOOST_AUTO_TEST_CASE ( USQHamiltonian_constructor ) {
+    
+    // Create single-particle basis
+    auto water = GQCP::Molecule::ReadXYZ("data/h2o.xyz");
+    const GQCP::SingleParticleBasis<double, GQCP::GTOShell> sp_basis (water, "STO-3G");
+    
+    // Create One- and SQTwoElectronOperators (and a transformation matrix) with compatible dimensions
+    size_t K = sp_basis.numberOfBasisFunctions();
+    GQCP::QCMatrix<double> H_core = GQCP::QCMatrix<double>::Random(K, K);
+
+    GQCP::QCRankFourTensor<double> g (K);
+    g.setRandom();
+
+   // Create SQ operators with greater dimensions
+    GQCP::QCMatrix<double> H_core_faulty = GQCP::QCMatrix<double>::Random(K+1, K+1);
+    GQCP::QCRankFourTensor<double> g_faulty (K+1);
+    g_faulty.setRandom();
+    // Create SQHamilonians with different dimensions
+    GQCP::SQHamiltonian<double> sq_hamiltonian_a (GQCP::ScalarSQOneElectronOperator<double>({H_core}), GQCP::ScalarSQTwoElectronOperator<double>({g}));
+    GQCP::SQHamiltonian<double> sq_hamiltonian_b (GQCP::ScalarSQOneElectronOperator<double>({H_core}), GQCP::ScalarSQTwoElectronOperator<double>({g}));
+    GQCP::SQHamiltonian<double> sq_hamiltonian_b_faulty (GQCP::ScalarSQOneElectronOperator<double>({H_core_faulty}), GQCP::ScalarSQTwoElectronOperator<double>({g_faulty}));
+
+    // Check if a correct constructor works with compatible elements
+    BOOST_CHECK_NO_THROW(GQCP::USQHamiltonian<double> (sq_hamiltonian_a, sq_hamiltonian_b, GQCP::ScalarSQTwoElectronOperator<double>({g})));
+    // Check if a constructor throws an error with incompatible elements
+    BOOST_CHECK_THROW(GQCP::USQHamiltonian<double> (sq_hamiltonian_a, sq_hamiltonian_b_faulty, GQCP::ScalarSQTwoElectronOperator<double>({g})), std::invalid_argument);
+    BOOST_CHECK_THROW(GQCP::USQHamiltonian<double> (sq_hamiltonian_a, sq_hamiltonian_b, GQCP::ScalarSQTwoElectronOperator<double>({g_faulty})), std::invalid_argument);
+   
+}
+
+
+BOOST_AUTO_TEST_CASE ( USQHamiltonian_transform ) {
+    
+    // This test will test if a total transformation or two individual transformations for the individual components of the USQHamiltonian amount to the same result
+    // Create single-particle basis for alpha and beta
+    auto water = GQCP::Molecule::ReadXYZ("data/h2o.xyz");
+    const GQCP::SingleParticleBasis<double, GQCP::GTOShell> sp_basis_a (water, "STO-3G");
+    const GQCP::SingleParticleBasis<double, GQCP::GTOShell> sp_basis_b (water, "STO-3G");
+
+    size_t K = sp_basis_a.numberOfBasisFunctions();
+
+    GQCP::USQHamiltonian<double> usq_hamiltonian1 = GQCP::USQHamiltonian<double>::Molecular(sp_basis_a, sp_basis_b, water);
+    GQCP::USQHamiltonian<double> usq_hamiltonian2 = GQCP::USQHamiltonian<double>::Molecular(sp_basis_a, sp_basis_b, water);
+
+    GQCP::SquareMatrix<double> U = GQCP::SquareMatrix<double>::RandomUnitary(K);
+
+    usq_hamiltonian1.transform(U);
+    usq_hamiltonian2.transformAlpha(U);
+    usq_hamiltonian2.transformBeta(U);
+
+    BOOST_CHECK(usq_hamiltonian1.twoElectronMixed().parameters().isApprox(usq_hamiltonian2.twoElectronMixed().parameters()));
+    BOOST_CHECK(usq_hamiltonian1.alphaHamiltonian().core().parameters().isApprox(usq_hamiltonian2.alphaHamiltonian().core().parameters()));
+    BOOST_CHECK(usq_hamiltonian1.betaHamiltonian().core().parameters().isApprox(usq_hamiltonian2.betaHamiltonian().core().parameters()));   
+}
+
