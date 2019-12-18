3ea1f8a56f4d7cbde3f62ab36e8bbe72302e3ffd
tmhuysen <tobias.huysentruyt@minimoa.net>
2019-12-18 10:20:47 +0100

    updated constrained drivers

--- gqcpy/tests/mullikenConstrained_binding_test.py
+++ gqcpy/tests/mullikenConstrained_binding_test.py
@@ -32,20 +32,32 @@ class MullikenConstrainedQCM(unittest.TestCase):
         self.reference_self_O_fragment_energy = -72.7239794944369
         self.reference_interaction_energy = -0.651220447214087
 
+        # Initiate a second set to test spin constraints
+        O_2b = gqcpy.Nucleus(8, 2, 0, 0)
+        NO_2b = gqcpy.Molecule([N,O_2b], +1) # The NO+ molecule with an intramolecular distance of 2 bohr
+
+        self.constrained_module_spin = gqcpy.MullikenConstrainedFCI(NO_2b, basis_set, basis_targets)
+        self.atomic_sz = 0.938336864443
+
     def tearDown(self):
         pass
 
     def test_properties(self):
         """ compare properties with reference """
         self.constrained_module.solveMullikenDavidson(self.lambda_input)
-        # self.assertAlmostEqual(self.constrained_module.get_energy(), self.reference_energy)
-        # self.assertAlmostEqual(self.constrained_module.get_population(), self.reference_population)
+        self.assertAlmostEqual(self.constrained_module.get_energy(), self.reference_energy)
+        self.assertAlmostEqual(self.constrained_module.get_population(), self.reference_population)
         #self.assertAlmostEqual(self.constrained_module.get_entropy(), self.reference_entropy) entropy is sensitive to degeneracies is disabled
         self.assertAlmostEqual(self.constrained_module.get_A_fragment_energy(), self.reference_N_fragment_energy)
         self.assertAlmostEqual(self.constrained_module.get_A_fragment_self_energy(), self.reference_self_N_fragment_energy)
         self.assertAlmostEqual(self.constrained_module.get_B_fragment_energy(), self.reference_O_fragment_energy)
         self.assertAlmostEqual(self.constrained_module.get_B_fragment_self_energy(), self.reference_self_O_fragment_energy)
         self.assertAlmostEqual(self.constrained_module.get_interaction_energy(), self.reference_interaction_energy)
+
+    def test_spinZ(self):
+        """ compare spin-z with reference """
+        self.constrained_module_spin.solveMullikenDavidson(0, 0.5)
+        self.assertAlmostEqual(self.constrained_module_spin.get_sz(), self.atomic_sz)
 
     
 if __name__ == '__main__':
--- include/QCMethod/MullikenConstrainedFCI.hpp
+++ include/QCMethod/MullikenConstrainedFCI.hpp
@@ -25,6 +25,7 @@
 #include "HamiltonianBuilder/FrozenCoreFCI.hpp"
 #include "HamiltonianParameters/AtomicDecompositionParameters.hpp"
 #include "Operator/SecondQuantized/SQHamiltonian.hpp"
+#include "Operator/SecondQuantized/USQHamiltonian.hpp"
 #include "RDM/RDMCalculator.hpp"
 
 
@@ -34,6 +35,7 @@ namespace QCMethod {
 
 /**
  *  A class that solves the FCI Hamiltonian given a perturbation in the form of a langragian multiplier and the Mulliken operator for a pre-specified set of basis functions
+ *  Additionally an atomic Sz perturbation can be applied as wel. 
  */
 class MullikenConstrainedFCI {
 private:
@@ -41,11 +43,14 @@ private:
     std::vector<size_t> basis_targets;
     Molecule molecule;
     RSpinorBasis<double, GTOShell> spinor_basis;
+    USpinorBasis<double, GTOShell> uspinor_basis;
     SQHamiltonian<double> sq_hamiltonian;
+    USQHamiltonian<double> usq_hamiltonian;
     std::string basis_set;  // the basisset that should be used
     FrozenProductFockSpace fock_space = FrozenProductFockSpace(0, 0, 0, 0); // Default
     FrozenCoreFCI fci = FrozenCoreFCI(FrozenProductFockSpace(0, 0, 0, 0)); 
     ScalarSQOneElectronOperator<double> mulliken_operator;
+    ScalarSQOneElectronOperator<double> sq_sz_operator;
     AtomicDecompositionParameters adp = AtomicDecompositionParameters();
     RDMCalculator rdm_calculator = RDMCalculator();
 
@@ -55,7 +60,10 @@ private:
     std::vector<double> energy;
     std::vector<double> population; 
     std::vector<double> lambda;
+    std::vector<double> lambda_sz;
     std::vector<double> entropy;
+    std::vector<double> sz;
+
 
     // Decomposed solutions
     std::vector<double> A_fragment_energy;
@@ -73,8 +81,9 @@ private:
      *  
      *  @param eigenpairs           the eigenpairs from the CI solver
      *  @param multiplier           the Lagrangian multiplier associated with the solution
+     *  @param sz_multiplier        a given multiplier for the atomic Sz constraint
      */ 
-    void parseSolution(const std::vector<Eigenpair>& eigenpairs, const double multiplier);
+    void parseSolution(const std::vector<Eigenpair>& eigenpairs, const double multiplier, const double sz_multiplier = 0);
 
     /**
      *  Throws an error if no solution is available
@@ -106,26 +115,29 @@ public:
     /**
      *  Solve the eigenvalue problem for a multiplier with the davidson algorithm
      *  
-     *  @param multiplier           a given multiplier
+     *  @param multiplier           a given multiplier for the Mulliken constraint
      *  @param guess                supply a davidson guess
+     *  @param sz_multiplier        a given multiplier for the atomic Sz constraint
      */
-    void solveMullikenDavidson(const double multiplier, const VectorX<double>& guess);
+    void solveMullikenDavidson(const double multiplier, const VectorX<double>& guess, const double sz_multiplier = 0);
 
     /**
      *  Solve the eigenvalue problem for a multiplier with the davidson algorithm, davidson guess will be the previously stored solution
      *  if none is available the Hartree Fock expansion will be used instead
      *  
      *  @param multiplier           a given multiplier
+     *  @param sz_multiplier        a given multiplier for the atomic Sz constraint
      */
-    void solveMullikenDavidson(const double multiplier);
+    void solveMullikenDavidson(const double multiplier, const double sz_multiplier = 0);
 
     /**
      *  Solve the eigenvalue problem for a the next multiplier dense
      * 
      *  @param multiplier           a given multiplier
-     *  @param nos                  the number of eigenpairs or "states" that should be stored for each multiplier
+     *  @param nos                  the number of eigenpairs or "states" that should be stored for each multiplier``
+     *  @param sz_multiplier        a given multiplier for the atomic Sz constraint
      */
-    void solveMullikenDense(const double multiplier, const size_t nos);
+    void solveMullikenDense(const double multiplier, const size_t nos, const double sz_multiplier = 0);
 
     /**
      *  @param index                refers to the index of the number of requested states 
@@ -135,7 +147,9 @@ public:
     double get_energy(const size_t index = 0) const { this->checkAvailableSolutions("get_energy"); return this->energy[index]; };
     double get_population(const size_t index = 0) const { this->checkAvailableSolutions("get_population"); return this->population[index]; };
     double get_lambda(const size_t index = 0) const { this->checkAvailableSolutions("get_lambda"); return this->lambda[index]; };
+    double get_lambda_sz(const size_t index = 0) const { this->checkAvailableSolutions("get_lambda_sz"); return this->lambda_sz[index]; };
     double get_entropy(const size_t index = 0) const { this->checkAvailableSolutions("get_entropy"); return this->entropy[index]; };
+    double get_sz(const size_t index = 0) const { this->checkAvailableSolutions("get_sz"); return this->sz[index]; };
     double get_A_fragment_energy(const size_t index = 0) const { this->checkAvailableSolutions("get_A_fragment_energy"); this->checkDiatomicMolecule("get_A_fragment_energy"); return this->A_fragment_energy[index]; };
     double get_A_fragment_self_energy(const size_t index = 0) const { this->checkAvailableSolutions("get_A_fragment_self_energy"); this->checkDiatomicMolecule("get_A_fragment_self_energy"); return this->A_fragment_self_energy[index]; };
     double get_B_fragment_energy(const size_t index = 0) const { this->checkAvailableSolutions("get_B_fragment_energy"); this->checkDiatomicMolecule("get_B_fragment_energy"); return this->B_fragment_energy[index]; };
@@ -150,7 +164,7 @@ public:
      *  @param index             refers to the index of the number of requested states 
      * 
      *  @return all properties in vector that contains:
-     *      energy, population (on the selected basis functions), lambda (or the multiplier), entropy
+     *      energy, population (on the selected basis functions), Sz, lambda (or the multiplier for the Mulliken constraint), lambda_sz (for atomic Sz), entropy
      *      if diatomic we additionally find: A_fragment_energy, A_fragment_self_energy, B_fragment_energy, B_fragment_self_energy and interaction_energy in that order.
      */
     std::vector<double> all_properties(const size_t index = 0) const; 
