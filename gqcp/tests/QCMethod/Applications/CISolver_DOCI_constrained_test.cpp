916fb046c79194b642c6a7acb1e3a279d0c14b7a
Laurent Lemmens <laurent_lemmens@hotmail.com>
2020-01-16 16:31:44 +0100

    Reinstate a DOCI constrained test.

--- gqcp/tests/QCMethod/Applications/CISolver_DOCI_constrained_test.cpp
+++ gqcp/tests/QCMethod/Applications/CISolver_DOCI_constrained_test.cpp
@@ -22,6 +22,7 @@
 #include "Basis/transform.hpp"
 #include "Basis/SpinorBasis/RSpinorBasis.hpp"
 #include "Operator/SecondQuantized/SQHamiltonian.hpp"
+#include "Operator/FirstQuantized/Operator.hpp"
 #include "Processing/Properties/expectation_values.hpp"
 #include "Processing/RDM/RDMCalculator.hpp"
 #include "QCMethod/CI/CISolver.hpp"
@@ -30,80 +31,81 @@
 #include "QCMethod/RHF/PlainRHFSCFSolver.hpp"
 
 
-// BOOST_AUTO_TEST_CASE ( CO_DOCI_constrained_dense ) {
-//     // Reference data from tmhuysens thesis
-//     Eigen::Matrix<double, 21 , 3> CO_data;
-//     //        lamda | C population | DOCI energy
-//     CO_data <<  -1.000000, 	5.80863087698, 	-111.281035626,
-//                 -0.900000, 	5.81016803318, 	-111.282495093,
-//                 -0.800000, 	5.8118219348 , 	-111.28389979 ,
-//                 -0.700000, 	5.81362985899, 	-111.285254278,
-//                 -0.600000, 	5.81563610261, 	-111.286556475,
-//                 -0.500000, 	5.81789473016, 	-111.28779636 ,
-//                 -0.400000, 	5.82047326851, 	-111.288953711,
-//                 -0.300000, 	5.82345780552, 	-111.289994495,
-//                 -0.200000, 	5.82696018148, 	-111.290865222,
-//                 -0.100000, 	5.83112832986, 	-111.291484159,
-//                  0.000000, 	5.83616141374, 	-111.291727603,
-//                  0.100000, 	5.84233235124, 	-111.291408185,
-//                  0.200000, 	5.85002183736, 	-111.29024015 ,
-//                  0.300000, 	5.85977033411, 	-111.28778306 ,
-//                  0.400000, 	5.87235795874, 	-111.283349644,
-//                  0.500000, 	5.88892639439, 	-111.275854656,
-//                  0.600000, 	5.91115885823, 	-111.263570726,
-//                  0.700000, 	5.94152162485, 	-111.243754333,
-//                  0.800000, 	5.98350566877, 	-111.212151981,
-//                  0.900000, 	6.0416010444 , 	-111.162616424,
-//                  1.000000, 	6.12030270656, 	-111.087663866;
+/**
+ *  Check if Mulliken-constrained DOCI works. The reference data is taken from @tmhuysen's Master's thesis.
+ */
+BOOST_AUTO_TEST_CASE ( CO_DOCI_constrained_dense ) {
 
-//     // Create the molecular Hamiltonian for CO
-//     auto CO = GQCP::Molecule::ReadXYZ("data/CO_mulliken.xyz");
-//     GQCP::RSpinorBasis<double, GQCP::GTOShell> spinor_basis (CO, "STO-3G");
-//     auto sq_hamiltonian = GQCP::SQHamiltonian<double>::Molecular(spinor_basis, CO);  // in an AO basis
+    Eigen::Matrix<double, 21 , 3> CO_data;  // lambda (multiplier), Mulliken population on C, DOCI energy
+    CO_data <<  -1.000000,  5.80863087698,  -111.281035626,
+                -0.900000,  5.81016803318,  -111.282495093,
+                -0.800000,  5.8118219348 ,  -111.28389979,
+                -0.700000,  5.81362985899,  -111.285254278,
+                -0.600000,  5.81563610261,  -111.286556475,
+                -0.500000,  5.81789473016,  -111.28779636,
+                -0.400000,  5.82047326851,  -111.288953711,
+                -0.300000,  5.82345780552,  -111.289994495,
+                -0.200000,  5.82696018148,  -111.290865222,
+                -0.100000,  5.83112832986,  -111.291484159,
+                 0.000000,  5.83616141374,  -111.291727603,
+                 0.100000,  5.84233235124,  -111.291408185,
+                 0.200000,  5.85002183736,  -111.29024015,
+                 0.300000,  5.85977033411,  -111.28778306,
+                 0.400000,  5.87235795874,  -111.283349644,
+                 0.500000,  5.88892639439,  -111.275854656,
+                 0.600000,  5.91115885823,  -111.263570726,
+                 0.700000,  5.94152162485,  -111.243754333,
+                 0.800000,  5.98350566877,  -111.212151981,
+                 0.900000,  6.0416010444 ,  -111.162616424,
+                 1.000000,  6.12030270656,  -111.087663866;
 
-//     // Create a plain RHF SCF solver and solve the SCF equations
-//     GQCP::DIISRHFSCFSolver diis_scf_solver (sq_hamiltonian, spinor_basis, CO);
-//     diis_scf_solver.solve();
-//     auto rhf = diis_scf_solver.get_solution();
+    // Create the molecular Hamiltonian for CO
+    auto CO = GQCP::Molecule::ReadXYZ("data/CO_mulliken.xyz");
+    GQCP::RSpinorBasis<double, GQCP::GTOShell> spinor_basis (CO, "STO-3G");
+    auto sq_hamiltonian = GQCP::SQHamiltonian<double>::Molecular(spinor_basis, CO);  // in an AO basis
 
-//     // Transform the Hamiltonian to the RHF orbital basis
-//     basisTransform(spinor_basis, sq_hamiltonian, rhf.get_C());
+    // Create a plain RHF SCF solver and solve the SCF equations
+    GQCP::DIISRHFSCFSolver diis_scf_solver (sq_hamiltonian, spinor_basis, CO);
+    diis_scf_solver.solve();
+    auto rhf = diis_scf_solver.get_solution();
 
-//     GQCP::FockSpace fock_space (sq_hamiltonian.dimension(), CO.numberOfElectrons()/2);  // dim = 4
+    // Transform the Hamiltonian to the RHF orbital basis
+    basisTransform(spinor_basis, sq_hamiltonian, rhf.get_C());
 
-//     // Create the DOCI module
-//     GQCP::DOCI doci (fock_space);
-//     GQCP::RDMCalculator rdm_calc (fock_space);
+    GQCP::FockSpace fock_space (sq_hamiltonian.dimension(), CO.numberOfElectrons()/2);  // dim = 4
 
-//     numopt::eigenproblem::DenseSolverOptions dense_solver_options;
-//     GQCP::Vectoru ao_list = {0,1,2,3,4};
+    // Create the DOCI module
+    GQCP::DOCI doci (fock_space);
+    GQCP::RDMCalculator rdm_calc (fock_space);
 
-//     for (int i = 0; i < 21; i++) {
-//         // Calculate the Mulliken operator
-//         auto mulliken_operator = spinor_basis.calculateMullikenOperator(ao_list);
+    GQCP::DenseSolverOptions dense_solver_options;
+    GQCP::Vectoru ao_list = {0,1,2,3,4};
 
-//         // Contrain the original Hamiltonian parameters
-//         auto constrained_ham_par = sq_hamiltonian.constrain(mulliken_operator, CO_data(i, 0));
+    for (int i = 0; i < 21; i++) {
+        // Calculate the Mulliken operator
+        auto mulliken_operator = spinor_basis.calculateMullikenOperator(ao_list);
 
-//         GQCP::CISolver ci_solver(doci, constrained_ham_par);
-//         // Solve Dense
-//         ci_solver.solve(dense_solver_options);
+        // Contrain the original Hamiltonian parameters
+        auto constrained_ham_par = sq_hamiltonian.constrain(mulliken_operator, CO_data(i, 0));
 
-//         // Retrieve the eigenvalues
-//         auto doci_dense_eigenvalue = ci_solver.get_eigenpair().get_eigenvalue();
-//         auto doci_dense_eigenvector = ci_solver.get_eigenpair().get_eigenvector();
+        GQCP::CISolver ci_solver (doci, constrained_ham_par);
+        ci_solver.solve(dense_solver_options);
 
-//         auto one_rdms = rdm_calc.calculate1RDMs(doci_dense_eigenvector);
+        // Retrieve the eigenvalues
+        auto doci_dense_eigenvalue = ci_solver.get_eigenpair().get_eigenvalue();
+        auto doci_dense_eigenvector = ci_solver.get_eigenpair().get_eigenvector();
 
-//         // Retrieve the expectation value of the Mulliken operator (aka the population)
-//         double mulliken_population = GQCP::calculateExpectationValue(mulliken_operator, one_rdms.one_rdm);
+        rdm_calc.set_coefficients(doci_dense_eigenvector);
+        auto one_rdms = rdm_calc.calculate1RDMs();
 
-//         // Retrieve the total energy by adding the lambda times the expectation value of the constraining operator
-//         double total_energy = doci_dense_eigenvalue + CO_data(i, 0) * mulliken_population + CO.calculateInternuclearRepulsionEnergy();
+        // Retrieve the expectation value of the Mulliken operator (aka the population)
+        double mulliken_population = GQCP::calculateExpectationValue(mulliken_operator, one_rdms.one_rdm)[0];
+
+        // Retrieve the total energy by adding the lambda times the expectation value of the constraining operator
+        double total_energy = doci_dense_eigenvalue + CO_data(i, 0) * mulliken_population + GQCP::Operator::NuclearRepulsion(CO).value();
 
 
-//         BOOST_CHECK(std::abs(total_energy - CO_data(i, 2)) < 1.0e-3);
-//         BOOST_CHECK(std::abs(mulliken_population - CO_data(i, 1)) < 1.0e-2);
-//     }
-//     BOOST_CHECK(true);
-// }
+        BOOST_CHECK(std::abs(total_energy - CO_data(i, 2)) < 1.0e-3);
+        BOOST_CHECK(std::abs(mulliken_population - CO_data(i, 1)) < 1.0e-2);
+    }
+}
