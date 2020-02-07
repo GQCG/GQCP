c514fbb1eb37974cf82304f5a94ce38d733c9a14
Laurent Lemmens <laurent_lemmens@hotmail.com>
2020-02-07 11:46:41 +0100

    Small clean-ups.

--- gqcp/include/gqcp.hpp
+++ gqcp/include/gqcp.hpp
@@ -112,9 +112,9 @@
 #include "Molecule/NuclearFramework.hpp"
 #include "Molecule/Nucleus.hpp"
 
-#include "QCModel/CI/SpinUnresolvedLinearExpansion.hpp"
 #include "QCModel/CI/LinearExpansion.hpp"
 #include "QCModel/CI/LinearExpansionReader.hpp"
+#include "QCModel/CI/SpinUnresolvedLinearExpansion.hpp"
 
 #include "ONVBasis/BaseONVBasis.hpp"
 #include "ONVBasis/BaseFrozenCoreONVBasis.hpp"
