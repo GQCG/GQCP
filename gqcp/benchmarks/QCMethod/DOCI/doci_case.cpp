9b06839cc82799134ef084e9134e703df537c058
Laurent Lemmens <laurent_lemmens@hotmail.com>
2020-02-17 15:22:29 +0100

    Change doci_case.

--- gqcp/benchmarks/QCMethod/DOCI/doci_case.cpp
+++ gqcp/benchmarks/QCMethod/DOCI/doci_case.cpp
@@ -4,39 +4,37 @@
 
 #include <benchmark/benchmark.h>
 
-#include "Mathematical/Optimization/Eigenproblem/Davidson/DavidsonSolver.hpp"
-#include "ONVBasis/SeniorityZeroONVBasis.hpp"
 #include "Operator/SecondQuantized/SQHamiltonian.hpp"
-#include "QCMethod/CI/CI.hpp"
-#include "QCMethod/CI/CIEnvironment.hpp"
+#include "QCMethod/CI/CISolver.hpp"
+#include "QCMethod/CI/HamiltonianBuilder/DOCI.hpp"
 
 
 static void test_case(benchmark::State& state) {
 
     const auto sq_hamiltonian = GQCP::SQHamiltonian<double>::ReadFCIDUMP("data/co_631g_klaas.FCIDUMP");
 
-    // Do the DOCI calculation by setting up a full seniority-zero ONV basis, a Davidson solver and a corresponding environment.
-    // The system contains 14 electrons and 28 basis functions.
+    // The system contains 14 electrons and requires 28 basis functions. For DOCI, we can use a SpinUnresolvedONV basis for 14 spatial orbitals and 7 electrons to represent this situation.  TODO: this should change: SpinUnresolvedONVBasis shouldn't be used for DOCI.
     const auto K = sq_hamiltonian.dimension();
     const auto N_P = 7;
+    const GQCP::SpinUnresolvedONVBasis onv_basis (K, N_P);  // dim = 1184040
 
-    const GQCP::SeniorityZeroONVBasis onv_basis (K, N_P);  // dim = 1184040
 
+    // Solve the DOCI eigenvalue problem with a Davidson solver
+    const GQCP::DOCI doci (onv_basis);
     const GQCP::VectorX<double> initial_guess = onv_basis.hartreeFockExpansion();
-    auto environment = GQCP::CIEnvironment::Iterative(sq_hamiltonian, onv_basis, initial_guess);
-    auto solver = GQCP::EigenproblemSolver::Davidson();
+    GQCP::DavidsonSolverOptions solver_options (initial_guess);
 
-
-    // Code inside this loop is measured repeatedly.
+    // Code inside this loop is measured repeatedly
     for (auto _ : state) {
-        const auto electronic_energy = GQCP::QCMethod::CI<GQCP::SeniorityZeroONVBasis>(onv_basis).optimize(solver, environment).groundStateEnergy();
+        GQCP::CISolver ci_solver (doci, sq_hamiltonian);
+        ci_solver.solve(solver_options);
 
-        benchmark::DoNotOptimize(electronic_energy);  // make sure that the variable is not optimized away by compiler
+        benchmark::DoNotOptimize(ci_solver);  // make sure the variable is not optimized away by compiler
     }
 
     state.counters["Spatial orbitals"] = K;
     state.counters["Electron pairs"] = N_P;
-    state.counters["Dimension"] = onv_basis.dimension();  // dim = 1184040
+    state.counters["Dimension"] = onv_basis.get_dimension();  // dim = 1184040
 }
 
 
