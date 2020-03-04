c5d049681b9d01b1e6beb66ddab3ae343cc05f77
Laurent Lemmens <laurent_lemmens@hotmail.com>
2020-03-04 10:06:28 +0100

    Enable a second test.

--- gqcp/tests/QCMethod/Geminals/QCMethod_AP1roG_test.cpp
+++ gqcp/tests/QCMethod/Geminals/QCMethod_AP1roG_test.cpp
@@ -57,18 +57,11 @@ BOOST_AUTO_TEST_CASE ( h2_631gdp ) {
     GQCP::basisTransform(spinor_basis, sq_hamiltonian, rhf_parameters.coefficientMatrix());
 
 
-    // Do an AP1roG calculation in that basis.
+    // Do an AP1roG calculation in that basis, using a zero initial guess.
     auto solver = GQCP::NonLinearEquationSolver<double>::Newton();
     auto environment = GQCP::PSEnvironment::AP1roG(sq_hamiltonian, N_P);
-    std::cout << "Created environment." << std::endl;
-    std::cout << environment.variables.back() << std::endl << std::endl;
-    const auto qc_method = GQCP::QCMethod::AP1roG(sq_hamiltonian, N_P);
+    const auto qc_structure = GQCP::QCMethod::AP1roG(sq_hamiltonian, N_P).optimize(solver, environment);
 
-    std::cout << "Created QC method." << std::endl;
-
-    const auto qc_structure = qc_method.optimize(solver, environment);
-
-    std::cout << "Created QC structure." << std::endl;
     const auto electronic_energy = qc_structure.groundStateEnergy();
     const auto ap1rog_coefficients = qc_structure.groundStateParameters().geminalCoefficients().asVector();  // column major
 
@@ -82,50 +75,47 @@ BOOST_AUTO_TEST_CASE ( h2_631gdp ) {
 }
 
 
+/**
+ *  Check an AP1roG calculation (with the weak interaction limit as an initial guess) with reference data from Ayers' implementation.
+ *  The test system is H2 with HF/6-31G** orbitals.
+ */
+BOOST_AUTO_TEST_CASE ( h2_631gdp_weak_interaction_limit ) {
+
+    // Input the reference data.
+    const double ref_ap1rog_energy = -1.8696828608304892;
+    GQCP::VectorX<double> ref_ap1rog_coefficients (9);
+    ref_ap1rog_coefficients << -0.05949796, -0.05454253, -0.03709503, -0.02899231, -0.02899231, -0.01317386, -0.00852702, -0.00852702, -0.00517996;
 
 
+    // Prepare molecular Hamiltonian in the canonical RHF basis.
+    const auto h2 = GQCP::Molecule::ReadXYZ("data/h2_olsens.xyz");
+    const auto N_P = h2.numberOfElectrons() / 2;
+    GQCP::RSpinorBasis<double, GQCP::GTOShell> spinor_basis (h2, "6-31G**");
+    auto sq_hamiltonian = GQCP::SQHamiltonian<double>::Molecular(spinor_basis, h2);  // in an AO basis
+
+    auto rhf_environment = GQCP::RHFSCFEnvironment<double>::WithCoreGuess(h2.numberOfElectrons(), sq_hamiltonian, spinor_basis.overlap().parameters());
+    auto plain_rhf_scf_solver = GQCP::RHFSCFSolver<double>::Plain();
+    const GQCP::DiagonalRHFFockMatrixObjective<double> objective (sq_hamiltonian);
+    const auto rhf_parameters = GQCP::QCMethod::RHF<double>().optimize(objective, plain_rhf_scf_solver, rhf_environment).groundStateParameters();
+
+    GQCP::basisTransform(spinor_basis, sq_hamiltonian, rhf_parameters.coefficientMatrix());
 
 
+    // Do an AP1roG calculation in that basis, using an initial guess being the weak interaction limit coefficients.
+    const auto G_initial = GQCP::AP1roGGeminalCoefficients::WeakInteractionLimit(sq_hamiltonian, N_P);
 
-// /**
-//  *  Check the solution of the AP1roG PSEs (with the weak interaction limit as initial guess) with reference data from Ayers' implementation
-//  *  The test system is H2 with HF/6-31G** orbitals
-//  */
-// BOOST_AUTO_TEST_CASE ( h2_631gdp_weak_interaction_limit ) {
+    auto solver = GQCP::NonLinearEquationSolver<double>::Newton();
+    auto environment = GQCP::PSEnvironment::AP1roG(sq_hamiltonian, G);
+    const auto qc_structure = GQCP::QCMethod::AP1roG(sq_hamiltonian, N_P).optimize(solver, environment);
 
-//     // Input the reference data
-//     const double ref_ap1rog_energy = -1.8696828608304892;
-//     GQCP::VectorX<double> ref_ap1rog_coefficients (9);
-//     ref_ap1rog_coefficients << -0.05949796, -0.05454253, -0.03709503, -0.02899231, -0.02899231, -0.01317386, -0.00852702, -0.00852702, -0.00517996;
+    const auto electronic_energy = qc_structure.groundStateEnergy();
+    const auto ap1rog_coefficients = qc_structure.groundStateParameters().geminalCoefficients().asVector();  // column major
 
 
-//     // Prepare molecular Hamiltonian in the RHF basis
-//     const auto h2 = GQCP::Molecule::ReadXYZ("data/h2_olsens.xyz");
-//     const auto N_P = h2.numberOfElectrons() / 2;
-//     GQCP::RSpinorBasis<double, GQCP::GTOShell> spinor_basis (h2, "6-31G**");
-//     auto sq_hamiltonian = GQCP::SQHamiltonian<double>::Molecular(spinor_basis, h2);  // in an AO basis
+    // Check the result.
+    BOOST_CHECK(std::abs(electronic_energy - ref_ap1rog_energy) < 1.0e-05);
 
-//     auto rhf_environment = GQCP::RHFSCFEnvironment<double>::WithCoreGuess(h2.numberOfElectrons(), sq_hamiltonian, spinor_basis.overlap().parameters());
-//     auto plain_rhf_scf_solver = GQCP::RHFSCFSolver<double>::Plain();
-//     const GQCP::DiagonalRHFFockMatrixObjective<double> objective (sq_hamiltonian);
-//     const auto rhf_parameters = GQCP::QCMethod::RHF<double>().optimize(objective, plain_rhf_scf_solver, rhf_environment).groundStateParameters();
-
-//     GQCP::basisTransform(spinor_basis, sq_hamiltonian, rhf_parameters.coefficientMatrix());
-
-
-//     // Solve the AP1roG PSEs, with the initial guess being the weak interaction limit coefficients
-//     const GQCP::AP1roGPSEs pses (sq_hamiltonian, N_P);
-//     const GQCP::AP1roGPSESolver ap1rog_pse_solver (pses);
-//     auto G = GQCP::AP1roGGeminalCoefficients::WeakInteractionLimit(sq_hamiltonian, N_P);
-//     ap1rog_pse_solver.solve(G);  // weak interaction limit coefficients are the initial guess
-
-//     const double electronic_energy = GQCP::QCModel::AP1roG::calculateEnergy(G, sq_hamiltonian);
-//     const GQCP::VectorX<double> ap1rog_coefficients = G.asVector();
-
-//     // Check the result
-//     BOOST_CHECK(std::abs(electronic_energy - ref_ap1rog_energy) < 1.0e-05);
-
-//     for (size_t i = 0; i < 9; i++) {
-//         BOOST_CHECK(std::abs(ap1rog_coefficients(i) - ref_ap1rog_coefficients(i)) < 1.0e-05);
-//     }
-// }
+    for (size_t i = 0; i < 9; i++) {
+        BOOST_CHECK(std::abs(ap1rog_coefficients(i) - ref_ap1rog_coefficients(i)) < 1.0e-05);
+    }
+}
