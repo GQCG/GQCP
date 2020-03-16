a4562d0c810e5ab630224e8340febfde05322da5
Xeno De Vriendt <xeno.dv@pandora.be>
2020-03-16 20:33:19 +0100

    adding tests for USQOneElectronOperator

--- gqcp/tests/Operator/SecondQuantized/USQOneElectronOperator_test.cpp
+++ gqcp/tests/Operator/SecondQuantized/USQOneElectronOperator_test.cpp
@@ -36,3 +36,49 @@ BOOST_AUTO_TEST_CASE ( USQOneElectronOperator_constructor ) {
     const GQCP::MatrixX<double> matrix = GQCP::MatrixX<double>::Zero(3, 4);
     BOOST_CHECK_THROW(GQCP::ScalarUSQOneElectronOperator<double> O2 {matrix}, std::invalid_argument);
 }
+
+/**
+ *  Check if the zero constructor actually sets its parameters to zeros
+ */
+BOOST_AUTO_TEST_CASE ( USQOneElectronOperator_zero_constructor ) {
+
+    const size_t dim = 2;
+    const GQCP::ScalarUSQOneElectronOperator<double> one_op {2};  // should initialize to zeros
+
+    BOOST_CHECK_EQUAL(one_op.alphaDimension(), dim);
+    BOOST_CHECK_EQUAL(one_op.betaDimension(), dim);
+    BOOST_CHECK(one_op.alphaParameters().isZero(1.0e-08));
+    BOOST_CHECK(one_op.betaParameters().isZero(1.0e-08));
+}
+
+
+/**
+ *  Check if addition of operators works as expected
+ */
+BOOST_AUTO_TEST_CASE ( USQOneElectronOperator_addition ) {
+
+    const size_t dim = 2;
+
+    // Initialize two test matrices and convert them into operators
+    GQCP::QCMatrix<double> M1 (dim);
+    M1 << 1.0, 2.0,
+          3.0, 4.0;
+    const GQCP::ScalarSQOneElectronOperator<double> op1 {M1};
+
+    GQCP::QCMatrix<double> M2 (dim);
+    M2 << 5.0, 6.0,
+          7.0, 8.0;
+    const GQCP::ScalarSQOneElectronOperator<double> op2 {M2};
+
+
+    // Initialize the reference and check the result
+    GQCP::QCMatrix<double> M_sum_ref (dim);
+    M_sum_ref <<  6.0,  8.0,
+                 10.0, 12.0;
+    
+    const auto op_sum = op1 + op2;
+    BOOST_CHECK(op_sum.alphaParameters().isApprox(M_sum_ref, 1.0e-08));
+    BOOST_CHECK(op_sum.betaParameters().isApprox(M_sum_ref, 1.0e-08));
+}
+
+
