a0d4a9f767f0fc7f83ceb502bd71ff79f1cc2ca7
dariatols <daria.tolstykh@ugent.be>
2020-06-13 15:11:55 +0200

    CCD Python bindings.

---
+++ gqcpy/src/QCModel/CC/CCD_bindings.cpp
@@ -0,0 +1,42 @@
+// This file is part of GQCG-GQCP.
+//
+// Copyright (C) 2017-2020  the GQCG developers
+//
+// GQCG-GQCP is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// GQCG-GQCP is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with GQCG-GQCP.  If not, see <http://www.gnu.org/licenses/>.
+
+#include "QCModel/CC/CCD.hpp"
+
+#include <pybind11/eigen.h>
+#include <pybind11/pybind11.h>
+
+
+namespace py = pybind11;
+
+
+namespace gqcpy {
+
+
+void bindQCModelCCD(py::module& module) {
+
+    py::class_<GQCP::QCModel::CCD<double>>(module, "QCModel_CCD", "The CCD wave function model.")
+
+        // PUBLIC METHODS
+        .def(
+            "t2Amplitudes",
+            &GQCP::QCModel::CCD<double>::t2Amplitudes,
+            "Return these CCD model parameters' T2-amplitudes");
+}
+
+
+}  // namespace gqcpy
