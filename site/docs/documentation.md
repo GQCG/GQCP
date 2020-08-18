0363d2b485a5738d277be966f4993a0c2e69eaa0
Laurent Lemmens <laurent_lemmens@hotmail.com>
2020-08-18 12:15:20 +0200

    Improve home page.

--- site/docs/documentation.md
+++ site/docs/documentation.md
@@ -5,13 +5,6 @@ sidebar_label: Overview
 ---
 
 
-## Why?
-
-At GQCG, we strongly believe that every complex problem can be solved. Calculating the electronic structure is one of them. From the combination of our passion for quantum chemistry and open-source software, GQCP saw its first light in 2017 and has been growing ever since. 
-
-But why does our quantum chemistry community need yet another electronic structure code? While other code bases might have better performance than GQCP, GQCP is modern at its core. GQCP is natively written in C++, so we have access to the most modern software techniques and compilers. GQCPY is its Python-bounded counterpart, so we fully embrace Python's present role as data manipulating language. Gone are the days of providing input files or writing executables, with GQCPY and Jupyter Notebooks you can naturally adapt a work flow that is both playful and systematic at the same time.
-
-
 ## General structure of GQCP
 
 GQCP has two main components: the C++ library `gqcp` and its associated Python bindings `gqcpy`. This is reflected in the folder structure of the GitHub repository, where we have, among other directories, the `gqcp` and `gqcpy` directories. As is common, the C++ library has `include`, `src` and `tests` folder, while the folder that contains the Python bindings only has a `src` folder and a `tests` folder.
--- site/website/pages/en/index.js
+++ site/website/pages/en/index.js
@@ -14,200 +14,204 @@ const Container = CompLibrary.Container;
 const GridBlock = CompLibrary.GridBlock;
 
 class HomeSplash extends React.Component {
-  render() {
-    const {siteConfig, language = ''} = this.props;
-    const {baseUrl, docsUrl} = siteConfig;
-    const docsPart = `${docsUrl ? `${docsUrl}/` : ''}`;
-    const langPart = `${language ? `${language}/` : ''}`;
-    const docUrl = (doc) => `${baseUrl}${docsPart}${langPart}${doc}`;
+    render() {
+        const { siteConfig, language = '' } = this.props;
+        const { baseUrl, docsUrl } = siteConfig;
+        const docsPart = `${docsUrl ? `${docsUrl}/` : ''}`;
+        const langPart = `${language ? `${language}/` : ''}`;
+        const docUrl = (doc) => `${baseUrl}${docsPart}${langPart}${doc}`;
 
-    const SplashContainer = (props) => (
-      <div className="homeContainer">
-        <div className="homeSplashFade">
-          <div className="wrapper homeWrapper">{props.children}</div>
-        </div>
-      </div>
-    );
+        const SplashContainer = (props) => (
+            <div className="homeContainer">
+                <div className="homeSplashFade">
+                    <div className="wrapper homeWrapper">{props.children}</div>
+                </div>
+            </div>
+        );
 
-    const Logo = (props) => (
-      <div className="projectLogo">
-        <img src={props.img_src} alt="Project Logo" />
-      </div>
-    );
+        const Logo = (props) => (
+            <div className="projectLogo">
+                <img src={props.img_src} alt="Project Logo" />
+            </div>
+        );
 
-    const ProjectTitle = (props) => (
-      <h2 className="projectTitle">
-        {props.title}
-        <small>{props.tagline}</small>
-      </h2>
-    );
+        const ProjectTitle = (props) => (
+            <h2 className="projectTitle">
+                {props.title}
+                <small>{props.tagline}</small>
+            </h2>
+        );
 
-    const PromoSection = (props) => (
-      <div className="section promoSection">
-        <div className="promoRow">
-          <div className="pluginRowBlock">{props.children}</div>
-        </div>
-      </div>
-    );
+        const PromoSection = (props) => (
+            <div className="section promoSection">
+                <div className="promoRow">
+                    <div className="pluginRowBlock">{props.children}</div>
+                </div>
+            </div>
+        );
 
-    const Button = (props) => (
-      <div className="pluginWrapper buttonWrapper">
-        <a className="button" href={props.href} target={props.target}>
-          {props.children}
-        </a>
-      </div>
-    );
+        const Button = (props) => (
+            <div className="pluginWrapper buttonWrapper">
+                <a className="button" href={props.href} target={props.target}>
+                    {props.children}
+                </a>
+            </div>
+        );
 
-    return (
-      <SplashContainer>
-        <Logo img_src={`${baseUrl}img/undraw_monitor.svg`} />
-        <div className="inner">
-          <ProjectTitle tagline={siteConfig.tagline} title={siteConfig.title} />
-          <PromoSection>
-            <Button href="#try">Try It Out</Button>
-            <Button href={docUrl('doc1.html')}>Example Link</Button>
-            <Button href={docUrl('doc2.html')}>Example Link 2</Button>
-          </PromoSection>
-        </div>
-      </SplashContainer>
-    );
-  }
+        return (
+            <SplashContainer>
+                <Logo img_src={`${baseUrl}img/undraw_monitor.svg`} />
+                <div className="inner">
+                    <ProjectTitle tagline={siteConfig.tagline} title={siteConfig.title} />
+                    <PromoSection>
+                        <Button href="#try">Try It Out</Button>
+                        <Button href={docUrl('doc1.html')}>Example Link</Button>
+                        <Button href={docUrl('doc2.html')}>Example Link 2</Button>
+                    </PromoSection>
+                </div>
+            </SplashContainer>
+        );
+    }
 }
 
 class Index extends React.Component {
-  render() {
-    const {config: siteConfig, language = ''} = this.props;
-    const {baseUrl} = siteConfig;
+    render() {
+        const { config: siteConfig, language = '' } = this.props;
+        const { baseUrl } = siteConfig;
 
-    const Block = (props) => (
-      <Container
-        padding={['bottom', 'top']}
-        id={props.id}
-        background={props.background}>
-        <GridBlock
-          align="center"
-          contents={props.children}
-          layout={props.layout}
-        />
-      </Container>
-    );
+        const Block = (props) => (
+            <Container
+                padding={['bottom', 'top']}
+                id={props.id}
+                background={props.background}>
+                <GridBlock
+                    align="center"
+                    contents={props.children}
+                    layout={props.layout}
+                />
+            </Container>
+        );
 
-    const FeatureCallout = () => (
-      <div
-        className="productShowcaseSection paddingBottom"
-        style={{textAlign: 'center'}}>
-        <h2>Feature Callout</h2>
-        <MarkdownBlock>These are features of this project</MarkdownBlock>
-      </div>
-    );
+        const SustainableResearch = () => (
+            <div
+                className="productShowcaseSection paddingBottom"
+                style={{ textAlign: 'left' }}>
+                <h2>Sustainable research & reproducible development</h2>
+                <MarkdownBlock>
+                    At GQCG, our activities are centered around electronic structure for molecules. We're trying to help create a community of quantum chemists that have a common mindset, suitable for (academic) research in the 21st century. We're focused on sustainable and reproducible research, which is why our software developments are all open-source.
 
-    const TryOut = () => (
-      <Block id="try">
-        {[
-          {
-            content:
-              'To make your landing page more attractive, use illustrations! Check out ' +
-              '[**unDraw**](https://undraw.co/) which provides you with customizable illustrations which are free to use. ' +
-              'The illustrations you see on this page are from unDraw.',
-            image: `${baseUrl}img/undraw_code_review.svg`,
-            imageAlign: 'left',
-            title: 'Wonderful SVG Illustrations',
-          },
-        ]}
-      </Block>
-    );
+                    In order to support our research and development, GQCP saw its first light in 2017 and has been growing ever since.
+                </MarkdownBlock>
+            </div>
+        );
 
-    const Description = () => (
-      <Block background="dark">
-        {[
-          {
-            content:
-              'This is another description of how this project is useful',
-            image: `${baseUrl}img/undraw_note_list.svg`,
-            imageAlign: 'right',
-            title: 'Description',
-          },
-        ]}
-      </Block>
-    );
+        const TryOut = () => (
+            <Block id="try">
+                {[
+                    {
+                        content:
+                            'To make your landing page more attractive, use illustrations! Check out ' +
+                            '[**unDraw**](https://undraw.co/) which provides you with customizable illustrations which are free to use. ' +
+                            'The illustrations you see on this page are from unDraw.',
+                        image: `${baseUrl}img/undraw_code_review.svg`,
+                        imageAlign: 'left',
+                        title: 'Wonderful SVG Illustrations',
+                    },
+                ]}
+            </Block>
+        );
 
-    const LearnHow = () => (
-      <Block background="light">
-        {[
-          {
-            content:
-              'Each new Docusaurus project has **randomly-generated** theme colors.',
-            image: `${baseUrl}img/undraw_youtube_tutorial.svg`,
-            imageAlign: 'right',
-            title: 'Randomly Generated Theme Colors',
-          },
-        ]}
-      </Block>
-    );
+        const Description = () => (
+            <Block background="dark">
+                {[
+                    {
+                        content:
+                            'This is another description of how this project is useful',
+                        image: `${baseUrl}img/undraw_note_list.svg`,
+                        imageAlign: 'right',
+                        title: 'Description',
+                    },
+                ]}
+            </Block>
+        );
 
-    const Features = () => (
-      <Block layout="fourColumn">
-        {[
-          {
-            content: 'This is the content of my feature',
-            image: `${baseUrl}img/undraw_react.svg`,
-            imageAlign: 'top',
-            title: 'Feature One',
-          },
-          {
-            content: 'The content of my second feature',
-            image: `${baseUrl}img/undraw_operating_system.svg`,
-            imageAlign: 'top',
-            title: 'Feature Two',
-          },
-        ]}
-      </Block>
-    );
+        const LearnHow = () => (
+            <Block background="light">
+                {[
+                    {
+                        content:
+                            'Each new Docusaurus project has **randomly-generated** theme colors.',
+                        image: `${baseUrl}img/undraw_youtube_tutorial.svg`,
+                        imageAlign: 'right',
+                        title: 'Randomly Generated Theme Colors',
+                    },
+                ]}
+            </Block>
+        );
 
-    const Showcase = () => {
-      if ((siteConfig.users || []).length === 0) {
-        return null;
-      }
+        const Features = () => (
+            <Block layout="fourColumn">
+                {[
+                    {
+                        title: "Python bindings",
+                        image: `${baseUrl}img/undraw_react.svg`,
+                        imageAlign: 'top',
+                        content: "We use [pybind11](https://github.com/pybind/pybind11) to generate Python bindings for our C++ library. Using `gqcpy` as a Python module, we can embrace Python's present role as a data manipulating language. Gone are the days of providing input files or writing executables, with GQCPY and Jupyter Notebooks you can naturally adapt a work flow that is both playful and systematic at the same time."
+                    },
+                    {
+                        title: 'C++ library',
+                        image: `${baseUrl}img/undraw_operating_system.svg`,
+                        imageAlign: 'top',
+                        content: "GQCP is modern at its core. It is natively written in C++, so we have access to state-of-the-art software techniques and compilers."
+                    },
+                ]}
+            </Block>
+        );
 
-      const showcase = siteConfig.users
-        .filter((user) => user.pinned)
-        .map((user) => (
-          <a href={user.infoLink} key={user.infoLink}>
-            <img src={user.image} alt={user.caption} title={user.caption} />
-          </a>
-        ));
+        const Showcase = () => {
+            if ((siteConfig.users || []).length === 0) {
+                return null;
+            }
 
-      const pageUrl = (page) =>
-        baseUrl + (language ? `${language}/` : '') + page;
+            const showcase = siteConfig.users
+                .filter((user) => user.pinned)
+                .map((user) => (
+                    <a href={user.infoLink} key={user.infoLink}>
+                        <img src={user.image} alt={user.caption} title={user.caption} />
+                    </a>
+                ));
 
-      return (
-        <div className="productShowcaseSection paddingBottom">
-          <h2>Who is Using This?</h2>
-          <p>This project is used by all these people</p>
-          <div className="logos">{showcase}</div>
-          <div className="more-users">
-            <a className="button" href={pageUrl('users.html')}>
-              More {siteConfig.title} Users
+            const pageUrl = (page) =>
+                baseUrl + (language ? `${language}/` : '') + page;
+
+            return (
+                <div className="productShowcaseSection paddingBottom">
+                    <h2>Who is Using This?</h2>
+                    <p>This project is used by all these people</p>
+                    <div className="logos">{showcase}</div>
+                    <div className="more-users">
+                        <a className="button" href={pageUrl('users.html')}>
+                            More {siteConfig.title} Users
             </a>
-          </div>
-        </div>
-      );
-    };
+                    </div>
+                </div>
+            );
+        };
 
-    return (
-      <div>
-        <HomeSplash siteConfig={siteConfig} language={language} />
-        <div className="mainContainer">
-          <Features />
-          <FeatureCallout />
-          <LearnHow />
-          <TryOut />
-          <Description />
-          <Showcase />
-        </div>
-      </div>
-    );
-  }
+        return (
+            <div>
+                <HomeSplash siteConfig={siteConfig} language={language} />
+                <div className="mainContainer">
+                    <SustainableResearch />
+                    <Features />
+                    <LearnHow />
+                    <TryOut />
+                    <Description />
+                    <Showcase />
+                </div>
+            </div>
+        );
+    }
 }
 
 module.exports = Index;
