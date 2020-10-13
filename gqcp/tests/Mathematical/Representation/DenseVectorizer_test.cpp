8b215d7ee1828f89c9f59cb26fe620d5413f8aec
xdvriend <xeno.dv@pandora.be>
2020-10-13 16:20:16 +0200

    added multi dimensional array test case to denseVectorizer_test

--- gqcp/tests/Mathematical/Representation/DenseVectorizer_test.cpp
+++ gqcp/tests/Mathematical/Representation/DenseVectorizer_test.cpp
@@ -28,19 +28,19 @@
 BOOST_AUTO_TEST_CASE(offset_row) {
 
     // To understand the offset method, let's take a look at an example using a 3x3 matrix
-    //
+
     // [[1, 2, 3],
     //  [4, 5, 6],
     //  [7, 8, 9]]
-    //
+
     // Using row-major order, this 2D array can be seen as:
     // [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    //
+
     // The strides for this matrix are [3, 1]
     // To move to the next row you skip three positions, to move to the next column you skip one position
 
     // Create an array containing the dimension of each axis
-    // Create a vectorizer with axis dimensions 2, thus representing a 2D array and give it the dimension of each axis with the previously created array
+    // Create a vectorizer with axis dimensions 2, thus representing a 2D array and define its axis dimensions with the previously created array
     const auto dimensions = std::array<size_t, 2> {3, 3};
     const auto test_vectorizer = GQCP::DenseVectorizer<2>(dimensions, GQCP::Ordering::RowMajor);
 
@@ -71,19 +71,19 @@ BOOST_AUTO_TEST_CASE(offset_row) {
 BOOST_AUTO_TEST_CASE(offset_column) {
 
     // To understand the offset method, let's take a look at an example using a 3x3 matrix
-    //
+
     // [[1, 2, 3],
     //  [4, 5, 6],
     //  [7, 8, 9]]
-    //
+
     // Using column-major order, this 2D array can be seen as:
     // [1, 4, 7, 2, 5, 8, 3, 6, 9]
-    //
+
     // The strides for this matrix are [1, 3]
     // To move to the next row you skip one position, to move to the next column you skip three positions
 
     // Create an array containing the dimension of each axis
-    // Create a vectorizer with axis dimensions 2, thus representing a 2D array and give it the dimension of each axis with the previously created array
+    // Create a vectorizer with axis dimensions 2, thus representing a 2D array and define its axis dimensions with the previously created array
     const auto dimensions = std::array<size_t, 2> {3, 3};
     const auto test_vectorizer = GQCP::DenseVectorizer<2>(dimensions);
 
@@ -96,4 +96,45 @@ BOOST_AUTO_TEST_CASE(offset_column) {
     const int ref_2 = 1;
     const std::array<size_t, 2> indices_2 = {1, 0};
     BOOST_CHECK(test_vectorizer.offset(indices_2) == ref_2);
+}
+
+/**
+ *  Check if the offset is calculated correctly when columnMajor ordering is used.
+ */
+BOOST_AUTO_TEST_CASE(offset_multi_dimensional) {
+
+    // The offset method can also be used on multi dimensional arrays, let's take a look at an example using a 3D array.
+
+    //  [[[ 0,  1,  2,  3],
+    //    [ 4,  5,  6,  7],
+    //    [ 8,  9, 10, 11]],
+    //   [[12, 13, 14, 15],
+    //    [16, 17, 18, 19],
+    //    [20, 21, 22, 23]]]
+
+    // Using column-major order, this 3D array can be seen as:
+    // [0, 4, 8, 1, 5, 9, 2, 6, 10, 3, 7, 11, 12, 16, 20, 13, 17, 21, 14, 18, 22, 15, 19, 23]
+
+    // The strides for this matrix are [12, 1, 4]
+    // To move to the next row you skip one position, to move to the next column you skip four positions and to move to the next inner array you move twelve positions
+
+    // Create an array containing the dimension of each axis
+    // Create a vectorizer with axis dimensions 3, thus representing a 3D array and define its axis dimensions with the previously created array
+    const auto dimensions = std::array<size_t, 3> {3, 4, 2};
+    const auto test_vectorizer = GQCP::DenseVectorizer<3>(dimensions);
+
+    // Move one inner array and check the offset
+    const int ref_1 = 12;
+    const std::array<size_t, 3> indices_1 = {0, 0, 1};
+    BOOST_CHECK(test_vectorizer.offset(indices_1) == ref_1);
+
+    // Move one column and two rows and check the offset
+    const int ref_2 = 5;
+    const std::array<size_t, 3> indices_2 = {2, 1, 0};
+    BOOST_CHECK(test_vectorizer.offset(indices_2) == ref_2);
+
+    // Move one row and three columns check the offset
+    const int ref_3 = 10;
+    const std::array<size_t, 3> indices_3 = {1, 3, 0};
+    BOOST_CHECK(test_vectorizer.offset(indices_3) == ref_3);
 }
