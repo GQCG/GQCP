ce9df7f9f675d5306bc95d7221855099484c7169
xdvriend <xeno.dv@pandora.be>
2020-11-20 13:23:21 +0100

    rename calculateFockOPeratorEigenvalues() to calculateExcitationEnergies() and adjust documentation

--- gqcp/include/QCModel/HF/GHF.hpp
+++ gqcp/include/QCModel/HF/GHF.hpp
@@ -309,9 +309,9 @@ public:
 
 
     /**
-     *  @return the eigenvalues of the one-electron Fock Operator as a matrix.
+     *  @return a matrix containing all the possible excitation energies of the wavefunction model.
      */
-    GQCP::MatrixX<Scalar> calculateFockOperatorEigenvalues() const {
+    GQCP::MatrixX<Scalar> calculateExcitationEnergies() const {
 
         // Create the orbital space to determine the loops.
         const auto orbital_space = this->orbitalSpace();
@@ -369,8 +369,8 @@ public:
         const auto g = gsq_hamiltonian.twoElectron().transformed(this->coefficientMatrix()).antisymmetrized().parameters();
 
         // The elements F_BA and F_IJ are the eigenvalues of the one-electron Fock operator.
-        // The calculateFockOperatorEigenvalues API can be used to find these values
-        const auto F_values = this->calculateFockOperatorEigenvalues();
+        // The calculateExcitationEnergies API can be used to find these values
+        const auto F_values = this->calculateExcitationEnergies();
 
         // The next step is to create the needed tensor slice.
         // Zero-initialize an occupied-virtual-occupied-virtual object.
