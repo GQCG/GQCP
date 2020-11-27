5aef60cc2fd87ecdee3899a5f76dab4095f90063
Laurent Lemmens <laurent_lemmens@hotmail.com>
2020-11-27 11:20:12 +0100

    Intermediate commit.

--- gqcp/tests/DensityMatrix/SpinResolved1DM_test.cpp
+++ gqcp/tests/DensityMatrix/SpinResolved1DM_test.cpp
@@ -1,65 +1,65 @@
-// // This file is part of GQCG-GQCP.
-// //
-// // Copyright (C) 2017-2020  the GQCG developers
-// //
-// // GQCG-GQCP is free software: you can redistribute it and/or modify
-// // it under the terms of the GNU Lesser General Public License as published by
-// // the Free Software Foundation, either version 3 of the License, or
-// // (at your option) any later version.
-// //
-// // GQCG-GQCP is distributed in the hope that it will be useful,
-// // but WITHOUT ANY WARRANTY; without even the implied warranty of
-// // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// // GNU Lesser General Public License for more details.
-// //
-// // You should have received a copy of the GNU Lesser General Public License
-// // along with GQCG-GQCP.  If not, see <http://www.gnu.org/licenses/>.
+// This file is part of GQCG-GQCP.
+//
+// Copyright (C) 2017-2020  the GQCG developers
+//
+// GQCG-GQCP is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// GQCG-GQCP is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with GQCG-GQCP.  If not, see <http://www.gnu.org/licenses/>.
 
-// #define BOOST_TEST_MODULE "SpinResolved1DM"
+#define BOOST_TEST_MODULE "SpinResolved1DM"
 
-// #include <boost/test/unit_test.hpp>
+#include <boost/test/unit_test.hpp>
 
-// #include "DensityMatrix/SpinResolved1DM.hpp"
-// #include "Mathematical/Optimization/Eigenproblem/EigenproblemSolver.hpp"
-// #include "ONVBasis/SpinResolvedONVBasis.hpp"
-// #include "Operator/SecondQuantized/SQHamiltonian.hpp"
-// #include "QCMethod/CI/CI.hpp"
-// #include "QCMethod/CI/CIEnvironment.hpp"
-
-
-// /**
-//  *  Check if the trace of the spin-resolved 1-DM yields the appropriate number of electrons.
-//  *
-//  *  The system of interested is H2O//STO-3G, with 7 spatial orbitals and a Fock space dimension of 441.
-//  */
-// BOOST_AUTO_TEST_CASE(trace) {
-
-//     // Set up the molecular Hamiltonian in a Löwdin-orthonormalized spinor basis.
-//     const auto molecule = GQCP::Molecule::ReadXYZ("data/h2o_Psi4_GAMESS.xyz");
-//     const size_t N_alpha = molecule.numberOfElectronPairs();
-//     const size_t N_beta = molecule.numberOfElectronPairs();
-//     const auto N = molecule.numberOfElectrons();
-
-//     GQCP::RSpinOrbitalBasis<double, GQCP::GTOShell> spinor_basis {molecule, "STO-3G"};
-//     const auto K = spinor_basis.numberOfSpatialOrbitals();
-//     spinor_basis.lowdinOrthonormalize();
-
-//     const auto sq_hamiltonian = GQCP::RSQHamiltonian<double>::Molecular(spinor_basis, molecule);
+#include "DensityMatrix/SpinResolved1DM.hpp"
+#include "Mathematical/Optimization/Eigenproblem/EigenproblemSolver.hpp"
+#include "ONVBasis/SpinResolvedONVBasis.hpp"
+#include "Operator/SecondQuantized/SQHamiltonian.hpp"
+#include "QCMethod/CI/CI.hpp"
+#include "QCMethod/CI/CIEnvironment.hpp"
 
 
-//     // Do a dense FCI calculation.
-//     const GQCP::SpinResolvedONVBasis onv_basis {K, N_alpha, N_beta};
+/**
+ *  Check if the trace of the spin-resolved 1-DM yields the appropriate number of electrons.
+ *
+ *  The system of interested is H2O//STO-3G, with 7 spatial orbitals and a Fock space dimension of 441.
+ */
+BOOST_AUTO_TEST_CASE(trace) {
 
-//     auto environment = GQCP::CIEnvironment::Dense(sq_hamiltonian, onv_basis);
-//     auto solver = GQCP::EigenproblemSolver::Dense();
+    // Set up the molecular Hamiltonian in a Löwdin-orthonormalized spinor basis.
+    const auto molecule = GQCP::Molecule::ReadXYZ("data/h2o_Psi4_GAMESS.xyz");
+    const size_t N_alpha = molecule.numberOfElectronPairs();
+    const size_t N_beta = molecule.numberOfElectronPairs();
+    const auto N = molecule.numberOfElectrons();
 
-//     const auto linear_expansion = GQCP::QCMethod::CI<GQCP::SpinResolvedONVBasis>(onv_basis).optimize(solver, environment).groundStateParameters();
+    GQCP::RSpinOrbitalBasis<double, GQCP::GTOShell> spinor_basis {molecule, "STO-3G"};
+    const auto K = spinor_basis.numberOfSpatialOrbitals();
+    spinor_basis.lowdinOrthonormalize();
+
+    const auto sq_hamiltonian = GQCP::RSQHamiltonian<double>::Molecular(spinor_basis, molecule);
 
 
-//     // Calculate the 1-DMs, calculate the traces and check if they match the expected result.
-//     const auto D = linear_expansion.calculateSpinResolved1DM();
+    // Do a dense FCI calculation.
+    const GQCP::SpinResolvedONVBasis onv_basis {K, N_alpha, N_beta};
 
-//     BOOST_CHECK(std::abs(D.alpha().trace() - N_alpha) < 1.0e-12);
-//     BOOST_CHECK(std::abs(D.beta().trace() - N_beta) < 1.0e-12);
-//     BOOST_CHECK(std::abs(D.orbitalDensity().trace() - (N_alpha + N_beta)) < 1.0e-12);
-// }
+    auto environment = GQCP::CIEnvironment::Dense(sq_hamiltonian, onv_basis);
+    auto solver = GQCP::EigenproblemSolver::Dense();
+
+    const auto linear_expansion = GQCP::QCMethod::CI<GQCP::SpinResolvedONVBasis>(onv_basis).optimize(solver, environment).groundStateParameters();
+
+
+    // Calculate the 1-DMs, calculate the traces and check if they match the expected result.
+    const auto D = linear_expansion.calculateSpinResolved1DM();
+
+    BOOST_CHECK(std::abs(D.alpha().trace() - N_alpha) < 1.0e-12);
+    BOOST_CHECK(std::abs(D.beta().trace() - N_beta) < 1.0e-12);
+    BOOST_CHECK(std::abs(D.orbitalDensity().trace() - (N_alpha + N_beta)) < 1.0e-12);
+}
