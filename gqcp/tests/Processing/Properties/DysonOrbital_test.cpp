7a124ea7a272518de8f8f297d66679967f6cb384
dariatols <Daria.Tolstykh@UGent.be>
2020-12-12 15:50:55 +0100

    Test for annihilation on alpha electron now up and running.

--- gqcp/tests/Processing/Properties/DysonOrbital_test.cpp
+++ gqcp/tests/Processing/Properties/DysonOrbital_test.cpp
@@ -25,7 +25,7 @@
 /**
  *  Test the algorithm for the Dyson amplitudes against manually calculated coefficients for two (normalized) toy wave functions.
  */
-BOOST_AUTO_TEST_CASE(dyson_amplitudes) {
+BOOST_AUTO_TEST_CASE(dyson_amplitudes_1) {
 
     // Set up the manually calculated references.
     GQCP::Vector<double, 2> reference_amplitudes_alpha {0.39739531532399996, 0.9116729926689999};
@@ -64,10 +64,10 @@ BOOST_AUTO_TEST_CASE(dyson_amplitudes) {
 /**
  *  Test the algorithm for the Dyson amplitudes against manually calculated coefficients for two (normalized) toy wave functions.
  */
-BOOST_AUTO_TEST_CASE(dyson_amplitude2) {
+BOOST_AUTO_TEST_CASE(dyson_amplitudes_2) {
 
     // Set up the manually calculated references.
-    //GQCP::Vector<double, 2> reference_amplitudes_alpha {0.39739531532399996, 0.9116729926689999};
+    GQCP::Vector<double, 3> reference_amplitudes_alpha {0.578739438503937, -0.11202006721497709, -0.8605609287217918};
     GQCP::Vector<double, 3> reference_amplitudes_beta = {0.8606702596667963, 0.0868282794535802, -0.8722314733220338};
 
 
@@ -80,22 +80,21 @@ BOOST_AUTO_TEST_CASE(dyson_amplitude2) {
     GQCP::VectorX<double> coeffs2 = GQCP::VectorX<double>::Zero(9);
     coeffs2 << 0.26297864, 0.47549281, 0.13096657, 0.11171302, 0.79625911, 0.03717573, 0.12209374, 0.17338261, 0.4164798;
 
-    const GQCP::SpinResolvedONVBasis onv_basis {K, N / 2, N / 2};  // The reference ONV basis.
-    //const GQCP::SpinResolvedONVBasis onv_basis_alpha {K, N / 2 - 1, N / 2};  // An ONV basis with one less alpha electron.
-    const GQCP::SpinResolvedONVBasis onv_basis_beta {K, N / 2, N / 2 - 1};  // An ONV basis with one less beta electron.
+    const GQCP::SpinResolvedONVBasis onv_basis {K, N / 2, N / 2};            // The reference ONV basis.
+    const GQCP::SpinResolvedONVBasis onv_basis_alpha {K, N / 2 - 1, N / 2};  // An ONV basis with one less alpha electron.
+    const GQCP::SpinResolvedONVBasis onv_basis_beta {K, N / 2, N / 2 - 1};   // An ONV basis with one less beta electron.
 
     const auto linear_expansion = GQCP::LinearExpansion<GQCP::SpinResolvedONVBasis>(onv_basis, coeffs1);
-    //const auto linear_expansion_alpha = GQCP::LinearExpansion<GQCP::SpinResolvedONVBasis>(onv_basis_alpha, coeffs2);
+    const auto linear_expansion_alpha = GQCP::LinearExpansion<GQCP::SpinResolvedONVBasis>(onv_basis_alpha, coeffs2);
     const auto linear_expansion_beta = GQCP::LinearExpansion<GQCP::SpinResolvedONVBasis>(onv_basis_beta, coeffs2);
 
 
     // Calculate the Dyson amplitudes for both situations (alpha-reference) and (beta-reference), and check with the manual calculations.
-    //const auto dyson_orbital_alpha = GQCP::DysonOrbital<double>::TransitionAmplitudes(linear_expansion, linear_expansion_alpha);
-    //const auto& dyson_coefficients_alpha = dyson_orbital_alpha.amplitudes();
+    const auto dyson_orbital_alpha = GQCP::DysonOrbital<double>::TransitionAmplitudes(linear_expansion, linear_expansion_alpha);
+    const auto& dyson_coefficients_alpha = dyson_orbital_alpha.amplitudes();
     const auto dyson_orbital_beta = GQCP::DysonOrbital<double>::TransitionAmplitudes(linear_expansion, linear_expansion_beta);
     const auto& dyson_coefficients_beta = dyson_orbital_beta.amplitudes();
 
-    //BOOST_CHECK(dyson_coefficients_alpha.isApprox(reference_amplitudes_alpha, 1.0e-06));
+    BOOST_CHECK(dyson_coefficients_alpha.isApprox(reference_amplitudes_alpha, 1.0e-06));
     BOOST_CHECK(dyson_coefficients_beta.isApprox(reference_amplitudes_beta, 1.0e-06));
-    //std::cout << dyson_coefficients_beta(2) << std::endl;
 }
