4e0ecb1539eee699160c323c85962333161328b3
guacke <Guillaume.Acke@UGent.be>
2021-03-03 19:09:51 +0100

    Debug: update Python bindings test such that it uses MKL.

--- gqcpy/tests/test_bindings.py
+++ gqcpy/tests/test_bindings.py
@@ -1,2 +1,21 @@
 import gqcpy
-dir(gqcpy)
+import numpy as np
+
+A = gqcpy.AdjacencyMatrix.Linear(4)
+
+t = 1.0
+U = 3.5
+H = gqcpy.HoppingMatrix(A, t, U)
+
+hubbard_hamiltonian = gqcpy.HubbardHamiltonian(H)
+
+K = 4  # number of sites
+N_P = 2  # number of electron pairs
+
+onv_basis = gqcpy.SpinResolvedONVBasis(K, N_P, N_P)
+solver = gqcpy.EigenproblemSolver.Dense()
+environment = gqcpy.CIEnvironment.Dense(hubbard_hamiltonian, onv_basis)
+
+energy = gqcpy.CI(onv_basis).optimize(solver, environment).groundStateEnergy()
+
+np.testing.assert_almost_equal(energy, -2.135871608231944)
