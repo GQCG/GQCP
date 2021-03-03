19fe2568fd09186b41c2205d15418e74152d7353
guacke <Guillaume.Acke@UGent.be>
2021-03-03 19:49:29 +0100

    Debug: point back to static MKL linking as EasyBuild does the same (np.show_config()).

--- cmake/FindMKL.cmake
+++ cmake/FindMKL.cmake
@@ -26,17 +26,23 @@ Hints
 include(FindPackageHandleStandardArgs)
 
 find_path(MKL_INCLUDE_DIR mkl.h HINTS $ENV{MKLROOT}/include)
-find_library(MKL_SINGLE_DYNAMIC_LIBRARY NAMES libmkl_rt.so libmkl_rt.dylib PATHS $ENV{MKLROOT}/lib $ENV{MKLROOT}/lib/intel64)
+find_library(MKL_INTERFACE_LIBRARY NAMES libmkl_intel_lp64.a PATHS $ENV{MKLROOT}/lib $ENV{MKLROOT}/lib/intel64)
+find_library(MKL_INTEL_THREAD NAMES libmkl_intel_thread.a PATHS $ENV{MKLROOT}/lib $ENV{MKLROOT}/lib/intel64)
+find_library(MKL_CORE_LIBRARY NAMES libmkl_core.a PATHS $ENV{MKLROOT}/lib $ENV{MKLROOT}/lib/intel64)
 get_filename_component(IOMP5_ROOT $ENV{MKLROOT}/.. ABSOLUTE) # The root of IOMP installation can be one directory above the MKL root.
 find_library(MKL_IOMP5 NAMES libiomp5.so libiomp5.dylib PATHS $ENV{MKLROOT}/lib $ENV{MKLROOT}/lib/intel64 ${IOMP5_ROOT}/lib ${IOMP5_ROOT}/lib/intel64) # This library should be dynamically linked.
 
-mark_as_advanced(MKL_INCLUDE_DIR MKL_SINGLE_DYNAMIC_LIBRARY MKL_IOMP5)
+mark_as_advanced(MKL_INCLUDE_DIR MKL_INTERFACE_LIBRARY MKL_INTEL_THREAD MKL_CORE_LIBRARY MKL_IOMP5)
 
 find_package_handle_standard_args(MKL REQUIRED_VARS
-    MKL_INCLUDE_DIR MKL_SINGLE_DYNAMIC_LIBRARY MKL_IOMP5) # sets MKL_FOUND
+    MKL_INCLUDE_DIR MKL_INTERFACE_LIBRARY MKL_INTEL_THREAD MKL_CORE_LIBRARY MKL_IOMP5) # sets MKL_FOUND
 
 if(MKL_FOUND AND NOT TARGET MKL::MKL)
     add_library(MKL::MKL INTERFACE IMPORTED)
     target_include_directories(MKL::MKL INTERFACE ${MKL_INCLUDE_DIR})
-    target_link_libraries(MKL::MKL INTERFACE  ${MKL_SINGLE_DYNAMIC_LIBRARY} ${MKL_IOMP5} pthread m dl)
+    if(NOT APPLE)
+        target_link_libraries(MKL::MKL INTERFACE  -Wl,-Bstatic -Wl,--start-group mkl_intel_lp64 mkl_intel_thread mkl_core -Wl,--end-group -Wl,-Bdynamic iomp5 pthread m dl)
+    else()
+        target_link_libraries(MKL::MKL INTERFACE ${MKL_INTERFACE_LIBRARY} ${MKL_INTEL_THREAD} ${MKL_CORE_LIBRARY} iomp5 pthread m dl)
+    endif()
 endif()
