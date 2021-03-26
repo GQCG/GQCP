cbdcce7108065084352a1aeb94e192d5e6813e6f
dariatols <Daria.Tolstykh@UGent.be>
2021-03-26 10:37:39 +0000

    Make gitSHA1 test more strict.

--- gqcpy/tests/test_version.py
+++ gqcpy/tests/test_version.py
@@ -16,7 +16,7 @@ class TestVersion(unittest.TestCase):
         self.assertIsNotNone(gqcpy.Version.fullVersion())
 
     def testGitSHA1(self):
-        self.assertIsNotNone(gqcpy.Version.gitSHA1())
+        self.assertContains(gqcpy.Version.gitSHA1(), "dirty")
 
 if __name__ == '__main__':
     unittest.main()
