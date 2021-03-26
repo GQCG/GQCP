d3293312961626c31867434a1a53eb43116d5015
dariatols <Daria.Tolstykh@UGent.be>
2021-03-26 11:17:48 +0000

    Retrieve git hash and add test on length of hash commit.

--- gqcpy/tests/test_version.py
+++ gqcpy/tests/test_version.py
@@ -16,7 +16,7 @@ class TestVersion(unittest.TestCase):
         self.assertIsNotNone(gqcpy.Version.fullVersion())
 
     def testGitSHA1(self):
-        self.assertContains(gqcpy.Version.gitSHA1(), "dirty")
+        self.assertEqual(len(gqcpy.Version.gitSHA1()), 40)
 
 if __name__ == '__main__':
     unittest.main()
