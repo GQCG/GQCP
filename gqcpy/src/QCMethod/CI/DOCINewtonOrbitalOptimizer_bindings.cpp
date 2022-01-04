9c9986253db1424ccf9f797d7dbb76b6f5fc65a7
xdvriend <xeno.dv@pandora.be>
2022-01-04 17:06:02 +0000

    add missing includes

--- gqcpy/src/QCMethod/CI/DOCINewtonOrbitalOptimizer_bindings.cpp
+++ gqcpy/src/QCMethod/CI/DOCINewtonOrbitalOptimizer_bindings.cpp
@@ -19,7 +19,9 @@
 #include "Mathematical/Algorithm/IterativeAlgorithm.hpp"
 #include "QCMethod/CI/DOCINewtonOrbitalOptimizer.hpp"
 
+#include <pybind11/eigen.h>
 #include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
 
 
 namespace gqcpy {
