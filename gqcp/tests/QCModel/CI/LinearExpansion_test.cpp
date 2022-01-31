d22717fe10f8f1e99e206f289b192cd6523812c6
dariatols <Daria.Tolstykh@UGent.be>
2022-01-31 13:55:15 +0100

    Other constructor for C_tensor.

--- gqcp/tests/QCModel/CI/LinearExpansion_test.cpp
+++ gqcp/tests/QCModel/CI/LinearExpansion_test.cpp
@@ -147,13 +147,14 @@ BOOST_AUTO_TEST_CASE(orbital_reduced_density_matrix) {
     // const auto rho = wfn.calculateSystemOrbitalRDM(system_onvs, environment_onvs);
     const auto C = wfn.tensorizeCoefficients(system_onvs, environment_onvs);
 
-    GQCP::MatrixX<double> C_ref {
-                        {wfn.coefficient(1), 0, wfn.coefficient(3), 0},
-                        {0, wfn.coefficient(0), 0, 0},
-                        {wfn.coefficient(2), 0, wfn.coefficient(4), 0},
-                        {0, 0, 0, wfn.coefficient(5)}
-
-    };
+    GQCP::MatrixX<double> C_ref {4, 4};
+    // clang-format off
+    C_ref << 
+        wfn.coefficient(1), 0, wfn.coefficient(3), 0,
+        0, wfn.coefficient(0), 0, 0,
+        wfn.coefficient(2), 0, wfn.coefficient(4), 0,
+        0, 0, 0, wfn.coefficient(5);
+    // clang-format on
 
     BOOST_CHECK(C.isApprox(C_ref));
 }
