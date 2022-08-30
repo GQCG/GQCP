9b7fd8fea6ac0b923cb8c2a8c1f64e2895ede598
xdvriend <xeno.dv@pandora.be>
2022-08-30 10:51:29 +0000

    add one zero UNOCI test + cleanup

--- gqcp/tests/QCMethod/NOCI/NOCI_test.cpp
+++ gqcp/tests/QCMethod/NOCI/NOCI_test.cpp
@@ -402,7 +402,7 @@ BOOST_AUTO_TEST_CASE(NOCI_complex_generalized) {
 }
 
 /**
- *  Test the UNOCI in the case where two zero overlap values are present..
+ *  Test the UNOCI in the case where two zero overlap values are present.
  */
 BOOST_AUTO_TEST_CASE(NOCI_unrestricted_two_zero) {
     // This test case is taken from a python prototype from @lelemmen and @johdvos.
@@ -417,24 +417,24 @@ BOOST_AUTO_TEST_CASE(NOCI_unrestricted_two_zero) {
     GQCP::SquareMatrix<double> state_1_a {3};
     GQCP::SquareMatrix<double> state_1_b {3};
     // clang-format off
-    state_1_a << 3.01612395e-01, -7.27196084e-15,  1.18334666e+00,
-               4.60055205e-01, -9.96503166e-01, -5.35359699e-01,
-               4.60055205e-01,  9.96503166e-01, -5.35359699e-01;
+    state_1_a << 0.301612395,  0.0        ,  1.18334666 ,
+                 0.460055205, -0.996503166, -0.535359699,
+                 0.460055205,  0.996503166, -0.535359699;
 
-    state_1_b << 6.38858962e-01, -1.04073944e+00, -2.27595721e-15,
-                 2.80666711e-01,  6.47678158e-01, -9.96503166e-01,
-                 2.80666711e-01,  6.47678158e-01,  9.96503166e-01;
+    state_1_b << 0.638858962, -1.04073944 ,  0.0        ,
+                 0.280666711,  0.647678158, -0.996503166,
+                 0.280666711,  0.647678158,  0.996503166;
     // clang-format on
     GQCP::SquareMatrix<double> state_2_a {3};
     GQCP::SquareMatrix<double> state_2_b {3};
     // clang-format off
     state_2_a <<  0.30161239,  0.63885896,  1.18334666,
-                  0.46005521,  0.28066671, -0.5353597,
-                  0.46005521,  0.28066671, -0.5353597;
+                  0.46005521,  0.28066671, -0.5353597 ,
+                  0.46005521,  0.28066671, -0.5353597 ;
 
-    state_2_b << -7.27196084e-15, -1.04073944e+00, -2.27595721e-15,
-                 -9.96503166e-01,  6.47678158e-01, -9.96503166e-01,
-                  9.96503166e-01,  6.47678158e-01,  9.96503166e-01;
+    state_2_b <<  0.0        , -1.04073944 ,  0.0        ,
+                 -0.996503166,  0.647678158, -0.996503166,
+                  0.996503166,  0.647678158,  0.996503166;
     // clang-format on
 
     // Transform the matrices to the correct transformation type.
@@ -459,6 +459,59 @@ BOOST_AUTO_TEST_CASE(NOCI_unrestricted_two_zero) {
 
     // In this test case, the alpha and beta 1DM's should be equal. We check this fact, to confirm that all aspects of the complex NOCI method in an unrestricted basis work as expected.
     const auto reference_energy = -1.36209;
+
+    // Check the energy versus the reference.
+    BOOST_CHECK(std::abs(reference_energy - NOCI_model.groundStateEnergy()) < 1e-6);
+}
+
+/**
+ *  Test the UNOCI in the case where one zero overlap value is present.
+ */
+BOOST_AUTO_TEST_CASE(NOCI_unrestricted_one_zero) {
+    // This test case is taken from a python prototype from @lelemmen and @johdvos.
+    // It was for H3, at 1.6Å internuclear distance for the STO-3G basis set.
+    const auto molecule = GQCP::Molecule::HRingFromDistance(3, 3.023561581760001);  // H3, 1.6Å apart.
+
+    // The unrestricted spin orbital basis is also needed, as we require the overlap operator in AO basis.
+    const GQCP::USpinOrbitalBasis<double, GQCP::GTOShell> spin_orbital_basis {molecule, "STO-3G"};
+    const auto S = spin_orbital_basis.overlap();
+
+    // Initialize two non-orthogonal "unrestricted states".
+    GQCP::SquareMatrix<double> state_1 {3};
+    // clang-format off
+    state_1 << 0.639638709, -0.801711730,  0.189534772,
+               0.0        ,  0.0        , -1.04297785 ,
+               0.639638715,  0.801711722,  0.189534786;
+    // clang-format on
+    GQCP::SquareMatrix<double> state_2 {3};
+    // clang-format off
+    state_2 <<  0.0,  0.667128998, -0.801711735,
+                1.0, -0.296315367,  0.0        ,
+                0.0,  0.667129020,  0.801711717;
+    // clang-format on
+
+    // Transform the matrices to the correct transformation type.
+    const auto basis_state_1 = GQCP::UTransformation<double> {GQCP::UTransformationComponent<double> {state_1}, GQCP::UTransformationComponent<double> {state_2}};
+    const auto basis_state_2 = GQCP::UTransformation<double> {GQCP::UTransformationComponent<double> {state_2}, GQCP::UTransformationComponent<double> {state_1}};
+
+    // Create a vector out of these three basis states.
+    std::vector<GQCP::UTransformation<double>> basis_vector {basis_state_1, basis_state_2};
+
+    // Create a non-orthogonal state basis, using the basis state vector, the overlap operator in AO basis and the number of occupied orbitals for alpha and beta.
+    const auto NOS_basis = GQCP::UNonOrthogonalStateBasis<double> {basis_vector, S, 2, 1};
+
+    // To evaluate the Hamiltonian in the non-orthogonal state basis, we need the second quantized Hamiltonian in AO basis.
+    const auto sq_hamiltonian = spin_orbital_basis.quantize(GQCP::FQMolecularHamiltonian(molecule));
+
+    // Create a dense solver and corresponding environment and put them together in the QCMethod.
+    auto environment = GQCP::NOCIEnvironment::Dense(sq_hamiltonian, NOS_basis, molecule);
+    auto solver = GQCP::GeneralizedEigenproblemSolver::Dense<double>();
+
+    // We do not specify the number of states, meaning we only request the ground state.
+    const auto NOCI_model = GQCP::QCMethod::NOCI<double, GQCP::UNonOrthogonalStateBasis<double>>(NOS_basis).optimize(solver, environment);
+
+    // In this test case, the alpha and beta 1DM's should be equal. We check this fact, to confirm that all aspects of the complex NOCI method in an unrestricted basis work as expected.
+    const auto reference_energy = -1.37371;
 
     // Check the energy versus the reference.
     BOOST_CHECK(std::abs(reference_energy - NOCI_model.groundStateEnergy()) < 1e-6);
