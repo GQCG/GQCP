4e9163d78d3140f33e1ce12bd0f23ba9844f48e2
dariatols <Daria.Tolstykh@UGent.be>
2023-03-01 13:41:36 +0000

    Add base functionality each child class of SimpleDomain should have.

--- gqcp/include/Domain/SimpleDomain.hpp
+++ gqcp/include/Domain/SimpleDomain.hpp
@@ -23,13 +23,52 @@
 
 namespace GQCP {
 
+/*
+ *  MARK: DomainTraits
+ */
 
+/**
+ *  A type that provides compile-time information on the type of elements in a Domain that is otherwise not accessible through a public class alias.
+ */
+template <typename DerivedDomain>
+struct DomainTraits {};
+
+/**
+ *  A type specifically designed to act as a parent class for `DiscreteDomain` and `ContinuousDomain` in order to share common functionality.
+ *
+ *  @tparam _DerivedDomain              The type of the domain that derives from this class, enabling CRTP and compile-time polymorphism.
+ */
 template <typename _DerivedDomain>
 class SimpleDomain:
     public CRTP<_DerivedDomain> {
 public:
     // The type of domain that derives from this class, enabling CRTP and compile-time polymorphism..
     using DerivedDomain = _DerivedDomain;
+    // The type of elements that are present in the domain: discrete or continuous.
+    using ElementType = typename DomainTraits<DerivedDomain>::ElementType;
+
+
+public:
+    /**
+     *  @param element        the element in the domain
+     *
+     *  @return whether the Domain contains `element'.
+     */
+    bool inDomain(const ElementType& element) const = 0;
+
+    /**
+     *  @param other        the other CartesianGTO
+     *
+     *  @return if this DerivedDomain is equal to the other DerivedDomain.
+     */
+    virtual bool operator==(const DerivedDomain& other) const = 0;
+
+    /**
+     *  @param other        the other CartesianGTO
+     *
+     *  @return if this DerivedDomain is not equal to the other DerivedDomain.
+     */
+    bool operator!=(DerivedDomain& other) const = 0;
 };
 
 
