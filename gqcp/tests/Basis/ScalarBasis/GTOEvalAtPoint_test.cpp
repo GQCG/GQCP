ed0ce7c3a0052b9147516b8472f4446f2415357a
emielvb <emiel.vd.b@hotmail.com>
2025-04-16 13:25:28 +0200

    add London AO eval point test

--- gqcp/tests/Basis/ScalarBasis/GTOEvalAtPoint_test.cpp
+++ gqcp/tests/Basis/ScalarBasis/GTOEvalAtPoint_test.cpp
@@ -57,46 +57,49 @@ BOOST_AUTO_TEST_CASE(eval_gto) {
     // compare with hardcoded reference data
     const std::vector<double> AO_vals_ref = {0.0054346, 0.0, 0.000939672, 0.000194685, -0.000584055, 0.002401113, 0.006665074};
     for (size_t i = 0; i < nao; i++) {
-        BOOST_TEST(AO_vals[i] == AO_vals_ref[i], tt::tolerance(0.000001));
+        BOOST_TEST(AO_vals[i] == AO_vals_ref[i], tt::tolerance(1e-6));
     }
 }
 
 
-// /**
-//  *  Check if the LondonGTO evaluation at a random point in space is identical to PySCF results.
-//  */
-//  BOOST_AUTO_TEST_CASE(eval_gto) {
+/**
+ *  Check if the London GTO evaluation at a random point in space is identical to PySCF results.
+ */
+ BOOST_AUTO_TEST_CASE(eval_london_gto) {
 
-//     // Create an STO-3G basisset on (a toy geometry of) H2O.
-//     const GQCP::Nucleus h1 {1, 0.0, 0.0, 0.0};
-//     const GQCP::Nucleus o {8, 1.0, 0.0, 0.0};
-//     const GQCP::Nucleus h2 {1, 2.0, 0.0, 0.0};
-//     const GQCP::Molecule molecule {{h1, o, h2}};
+    // Create an STO-3G basisset on (a toy geometry of) H2O.
+    const GQCP::Nucleus h1 {1, 0.0, 0.0, 0.0};
+    const GQCP::Nucleus o {8, 1.0, 0.0, 0.0};
+    const GQCP::Nucleus h2 {1, 2.0, 0.0, 0.0};
+    const GQCP::Molecule molecule {{h1, o, h2}};
 
-//     // init zero-field because that's the only reference data we have
-//     const auto B = GQCP::HomogeneousMagneticField {{0.0, 0.0, 0.0}};  // Gauge origin at cartesian origin.
+    // zero-field because PySCF does not have field-dependent AOs
+    const auto B = GQCP::HomogeneousMagneticField {{0.0, 0.0, 0.0}};  // Gauge origin at cartesian origin.
+    // init basis
+    auto spin_orbital_basis = GQCP::RSpinOrbitalBasis<GQCP::complex, GQCP::LondonGTOShell> {molecule, "STO-3G", B};
+    // gather AOs
+    // each spatial orbital ("spatial MO") is expanded in the AO basis set functions.
+    const auto AOs = spin_orbital_basis.spatialOrbitals()[0].functions();
+    const int nao = AOs.size();
 
-//     // init basis
-//     const auto spin_orbital_basis = GQCP::RSpinOrbitalBasis<double, GQCP::GTOShell> {molecule, "STO-3G"};
-//     // gather AOs
-//     // each spatial orbital ("spatial MO") is expanded in the AO basis set functions.
-//     const auto AOs = spin_orbital_basis.spatialOrbitals()[0].functions();
-//     const int nao = AOs.size();
+    // init random point in space
+    const GQCP::Vector<double, 3> r = {1.3, -0.9, 3.7};
 
-//     // init random point in space
-//     const GQCP::Vector<double, 3> r = {1.3, -0.9, 3.7};
+    // init vector
+    std::vector<std::complex<double>> AO_vals;
+    AO_vals.reserve(nao);
+    // loop through AOs
+    for (size_t i = 0; i < nao; i++) {
+        // gather value at r, put it in vector
+        AO_vals.push_back(AOs[i](r));
+    }
 
-//     // init vector
-//     std::vector<double> AO_vals;
-//     AO_vals.reserve(nao);
-//     // loop through AOs
-//     for (size_t i = 0; i < nao; i++) {
-//         // gather value at r, put it in vector
-//         AO_vals.push_back(AOs[i](r));
-//     }
-//     // std::cout << AO_vals. << std::endl;
-
-//     // compare with hardcoded reference data
-//     const std::vector<double> AO_vals_ref = {0.0054346, 0.0, 0.000939672, 0.000194685, -0.000584055, 0.002401113, 0.006665074};
-//     BOOST_CHECK(AO_vals == AO_vals_ref);
-// }
+    // compare with hardcoded reference data
+    const std::vector<std::complex<double>> AO_vals_ref = {
+        {0.0054346, 0.0}, {0.0, 0.0}, {0.000939672, 0.0}, {0.000194685, 0.0}, {-0.000584055, 0.0}, {0.002401113, 0.0}, {0.006665074, 0.0}
+    };
+    for (size_t i = 0; i < nao; ++i) {
+        BOOST_TEST(AO_vals[i].real() == AO_vals_ref[i].real(), tt::tolerance(1e-6));
+        BOOST_TEST(AO_vals[i].imag() == AO_vals_ref[i].imag(), tt::tolerance(1e-6));
+    }
+}
