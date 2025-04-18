edfa379658045d1f2e922a0e9db07ea7792f86fe
emielvb <emiel.vd.b@hotmail.com>
2025-04-18 15:09:51 +0200

    first try for GradAO convenience function

--- gqcp/sandbox/eval_ao_at_point_development.cpp
+++ gqcp/sandbox/eval_ao_at_point_development.cpp
@@ -2,6 +2,7 @@
 #include "Basis/ScalarBasis/GTOShell.hpp"
 #include "Basis/ScalarBasis/ShellSet.hpp"
 #include "Mathematical/Functions/CartesianGTO.hpp"
+#include "Mathematical/Functions/LondonCartesianGTO.hpp"
 #include "Mathematical/Functions/EvaluableLinearCombination.hpp"
 #include "Basis/SpinorBasis/RSpinOrbitalBasis.hpp"
 #include <iostream>
@@ -10,6 +11,36 @@
 #include <typeinfo>
 #include <cxxabi.h>
 
+
+std::vector<std::vector<GQCP::complex, 3>> evalGradBasisSetAtPoint(const GQCP::Vector<double, 3>& r) const {
+        // gather basis set AOs from the first spatial orbital (which could eg be a spatial MO)
+        // which is expanded in the scalar basis set
+        const auto AOs = this->spatialOrbitals()[0].functions();
+        // init vector in which to gather each AO's value at r
+        std::vector<std::vector<GQCP::complex, 3>> grad_AO_vals;
+        grad_AO_vals.reserve(this->numberOfSpatialOrbitals()); //n_AO = n_MO
+        std::vector<Vector<EvaluableLinearCombination<GQCP::complex, EvaluableLinearCombination<GQCP::complex, LondonCartesianGTO>>, 3>> basis_function_gradients {this->numberOfSpatialOrbitals()};
+
+        // loop through AOs
+        for (size_t i= 0; i < this->numberOfSpatialOrbitals(); i++) {
+            const auto& basis_function = AOs[i];
+            // loop through its primitives
+            const auto contraction_length = basis_function.length();
+            const auto& contraction_coefficients = basis_function.coefficients();
+            const auto& primitives = basis_function.functions();
+
+            for (size_t d = 0; d < contraction_length; d++) {
+                const auto& contraction_coefficient = contraction_coefficients[d];
+                const auto primitive_gradient = primitives[d].calculatePositionGradient();
+                grad_AO_vals[i].append(contraction_coefficient, primitive_gradient(m));
+            }
+
+            // evaluate value at r, put it in vector
+            grad_AO_vals.push_back(grad_AO_vals[i](r));
+        }
+        
+        return AO_vals;
+}
 
 int main() {
 
@@ -34,6 +65,10 @@ int main() {
     for (size_t i = 0; i < ao_values.size(); ++i) {
         std::cout << "AO[" << i << "] = " << ao_values[i] << std::endl;
     }
+
+    // get gradient of LAOs.
+
+
 
     return 0;
 }
